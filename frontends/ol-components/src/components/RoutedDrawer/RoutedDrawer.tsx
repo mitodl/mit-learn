010621aaaca117ab018fd8e1c3c90516e13e6979
Carey Gumaer <gumaerc@mit.edu>
2024-10-18 12:24:51 -0400

    new width and title section

--- frontends/ol-components/src/components/RoutedDrawer/RoutedDrawer.tsx
+++ frontends/ol-components/src/components/RoutedDrawer/RoutedDrawer.tsx
@@ -11,11 +11,20 @@ import {
 } from "next/navigation"
 import { useToggle } from "ol-utilities"
 
-const closeSx: React.CSSProperties = {
-  position: "absolute",
-  top: "16px",
-  right: "22px",
-}
+const CloseButton = styled(ActionButton)(({ theme }) => ({
+  "&&&": {
+    position: "absolute",
+    top: "24px",
+    right: "32px",
+    backgroundColor: theme.custom.colors.lightGray2,
+    color: theme.custom.colors.black,
+    ["&:hover"]: {
+      opacity: 1,
+      backgroundColor: theme.custom.colors.red,
+      color: theme.custom.colors.white,
+    },
+  },
+}))
 
 const CloseIcon = styled(RiCloseLargeLine)`
   &&& {
@@ -111,15 +120,14 @@ const RoutedDrawer = <K extends string, R extends K = K>(
               params: childParams as Record<K, string>,
               closeDrawer: setOpen.off,
             })}
-          <ActionButton
-            style={closeSx}
+          <CloseButton
             variant="text"
             size="medium"
             onClick={setOpen.off}
             aria-label="Close"
           >
             <CloseIcon />
-          </ActionButton>
+          </CloseButton>
         </>
       }
     </Drawer>
