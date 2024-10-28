1dcd7ee372c81919bd65d2ff1d133f626b9ba1b1
Matt Bertrand <mrbertrand@gmail.com>
2024-10-28 10:50:47 -0400

    .

--- openapi/urls.py
+++ openapi/urls.py
@@ -36,4 +36,17 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="v1_schema"),
         name="v1_redoc",
     ),
+    path(
+        "api/v2/schema/", SpectacularAPIView.as_view(api_version="v2"), name="v2_schema"
+    ),
+    path(
+        "api/v2/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="v2_schema"),
+        name="v2_swagger_ui",
+    ),
+    path(
+        "api/v2/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="v2_schema"),
+        name="v2_redoc",
+    ),
 ]
