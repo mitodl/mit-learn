7875d01c0d25ea51387ebc1222bba869beec8d9c
shankar ambady <ambady@mit.edu>
2024-10-28 15:00:44 -0400

    removing text embedding field and coercing description field to valid value

--- learning_resources_search/serializers.py
+++ learning_resources_search/serializers.py
@@ -111,13 +111,6 @@ def get_resource_age_date(learning_resource_obj, resource_category):
     return resource_age_date
 
 
-def generate_embedding_text(resource_obj):
-    """
-    Combine attributes that will be used for vector embeddings
-    """
-    return f"{resource_obj.title} {resource_obj.description}"
-
-
 def serialize_learning_resource_for_update(
     learning_resource_obj: LearningResource,
 ) -> dict:
@@ -163,11 +156,11 @@ def serialize_learning_resource_for_update(
     is_incomplete_or_stale = (
         resource_age_date and resource_age_date.year <= STALENESS_CUTOFF
     ) or (learning_resource_obj.completeness < COMPLETENESS_CUTOFF)
-    embedding_text = generate_embedding_text(learning_resource_obj)
+    description_text = serialized_data.get("description", "")
+    serialized_data["description"] = description_text or None
     return {
         "resource_relations": {"name": "resource"},
         "created_on": learning_resource_obj.created_on,
-        "embedding_text": embedding_text,
         "is_learning_material": serialized_data["resource_category"]
         == LEARNING_MATERIAL_RESOURCE_CATEGORY,
         "resource_age_date": resource_age_date,
