b58ff51961b314d8931a10501c6d7e3384020681
shankar ambady <ambady@mit.edu>
2024-10-28 20:24:54 -0400

    adding fix for pruning percolate queries

--- learning_resources_search/utils.py
+++ learning_resources_search/utils.py
@@ -32,10 +32,12 @@ def prune_channel_subscriptions():
             percolate_serializer.get_search_request_data()
             | {"endpoint": LEARNING_RESOURCE}
         )
-        actual_query, _ = PercolateQuery.objects.get_or_create(
+        actual_query, _ = PercolateQuery.objects.update_or_create(
             source_type=PercolateQuery.CHANNEL_SUBSCRIPTION_TYPE,
             original_query=adjusted_original_query,
-            query=adjust_query_for_percolator(adjusted_original_query),
+            defaults={
+                "query": adjust_query_for_percolator(adjusted_original_query),
+            },
         )
         queries = PercolateQuery.objects.filter(
             original_query__contains=urllib.parse.parse_qs(query_string),
