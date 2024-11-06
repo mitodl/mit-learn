fc4dcb76094bcfd0a9017158fe8c4ea65d658d60
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2024-11-06 20:52:23 +0100

    Full prefetch for homepage (commented)

--- frontends/main/src/app/page.tsx
+++ frontends/main/src/app/page.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import HomePage from "@/app-pages/HomePage/HomePage"
 import { getMetadataAsync } from "@/common/metadata"
 import { QueryClient, dehydrate, Hydrate } from "@tanstack/react-query"
+// import { learningResourcesKeyFactory } from "api/hooks/learningResources"
 import { testimonialsKeyFactory } from "api/hooks/testimonials"
 import {
   NewsEventsListFeedTypeEnum,
@@ -28,10 +29,69 @@ const Page: React.FC = async () => {
   const queryClient = new QueryClient()
 
   await Promise.all([
-    /* We can't do this until resources are fully public, https://github.com/mitodl/hq/issues/5159 */
-    // queryClient.prefetchQuery(
-    //   learningResourcesKeyFactory.topics({ is_toplevel: true }),
-    // ),
+    /* We can't prefetch any learning resource until they are fully public, https://github.com/mitodl/hq/issues/5159
+
+    // The queries for carousel content are not checked in the query cache warnings as the use the key factory methods directly
+
+    // Featured Courses carousel "All"
+    queryClient.prefetchQuery(
+      learningResourcesKeyFactory.featured({
+        limit: 12,
+      }),
+    ),
+    // Featured Courses carousel "Free" (worth fetching for tabs not shown on load?)
+    queryClient.prefetchQuery(
+      learningResourcesKeyFactory.featured({
+        limit: 12,
+        free: true,
+      }),
+    ),
+    // Featured Courses carousel "With Certificate"
+    queryClient.prefetchQuery(
+      learningResourcesKeyFactory.featured({
+        limit: 12,
+        certification: true,
+        professional: false,
+      }),
+    ),
+    // Featured Courses carousel "Professional & Executive Learning"
+    queryClient.prefetchQuery(
+      learningResourcesKeyFactory.featured({
+        limit: 12,
+        certification: true,
+        professional: false,
+      }),
+    ),
+    // Media carousel "All"
+    queryClient.prefetchQuery(
+      learningResourcesKeyFactory.list({
+        resource_type: ["video", "podcast_episode"],
+        limit: 12,
+        sortby: "new",
+      }),
+    ),
+    // Media carousel "Videos"
+    queryClient.prefetchQuery(
+      learningResourcesKeyFactory.list({
+        resource_type: ["video"],
+        limit: 12,
+        sortby: "new",
+      }),
+    ),
+    // Media carousel "Podcasts"
+    queryClient.prefetchQuery(
+      learningResourcesKeyFactory.list({
+        resource_type: ["podcast_episode"],
+        limit: 12,
+        sortby: "new",
+      }),
+    ),
+    // Browse by Topic
+    queryClient.prefetchQuery(
+      learningResourcesKeyFactory.topics({ is_toplevel: true }),
+    ),
+    */
+
     queryClient.prefetchQuery(testimonialsKeyFactory.list({ position: 1 })),
     queryClient.prefetchQuery(
       newsEventsKeyFactory.list({
