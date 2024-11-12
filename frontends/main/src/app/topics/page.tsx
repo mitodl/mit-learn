d3b7fe38323f567f3d416ab7ec0c5bfefc1243c4
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2024-11-12 22:07:56 +0100

    Prefetch topics page

--- frontends/main/src/app/topics/page.tsx
+++ frontends/main/src/app/topics/page.tsx
@@ -1,5 +1,9 @@
 import React from "react"
 import { Metadata } from "next"
+import { Hydrate } from "@tanstack/react-query"
+import { prefetch } from "api/ssr/prefetch"
+import { learningResourcesKeyFactory } from "api/hooks/learningResources"
+import { channelsKeyFactory } from "api/hooks/channels"
 
 import { standardizeMetadata } from "@/common/metadata"
 export const metadata: Metadata = standardizeMetadata({
@@ -8,8 +12,17 @@ export const metadata: Metadata = standardizeMetadata({
 
 import TopicsListingPage from "@/app-pages/TopicsListingPage/TopicsListingPage"
 
-const Page: React.FC = () => {
-  return <TopicsListingPage />
+const Page: React.FC = async () => {
+  const dehydratedState = await prefetch([
+    learningResourcesKeyFactory.topics({}),
+    channelsKeyFactory.countsByType("topic"),
+  ])
+
+  return (
+    <Hydrate state={dehydratedState}>
+      <TopicsListingPage />
+    </Hydrate>
+  )
 }
 
 export default Page
