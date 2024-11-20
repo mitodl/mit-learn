ffc8837b63b07fe827c69d101bd7b5221f3e567d
Carey Gumaer <gumaerc@mit.edu>
2024-11-20 18:12:32 -0500

    add tests

--- frontends/api/src/test-utils/urls.ts
+++ frontends/api/src/test-utils/urls.ts
@@ -81,6 +81,9 @@ const learningResources = {
     `${API_BASE_URL}/api/v1/featured/${query(params)}`,
   similar: (params: Params<LRApi, "learningResourcesSimilarList">) =>
     `${API_BASE_URL}/api/v1/learning_resources/${params.id}/similar/`,
+  vectorSimilar: (
+    params: Params<LRApi, "learningResourcesVectorSimilarList">,
+  ) => `${API_BASE_URL}/api/v1/learning_resources/${params.id}/vector_similar/`,
   setLearningPathRelationships: (
     params?: Params<LRApi, "learningResourcesLearningPathsPartialUpdate">,
   ) =>
