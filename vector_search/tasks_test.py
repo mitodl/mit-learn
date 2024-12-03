dceac7db6c3130ea7c954de0a5f795a5d999c559
shankar ambady <ambady@mit.edu>
2024-12-03 14:07:20 -0500

    adding test for new celery task

--- vector_search/tasks_test.py
+++ vector_search/tasks_test.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 from django.conf import settings
 
@@ -5,12 +7,15 @@ from learning_resources.etl.constants import ETLSource
 from learning_resources.factories import (
     ContentFileFactory,
     CourseFactory,
+    LearningResourceFactory,
     ProgramFactory,
 )
+from learning_resources.models import LearningResource
 from learning_resources_search.constants import (
     COURSE_TYPE,
 )
-from vector_search.tasks import start_embed_resources
+from main.utils import now_in_utc
+from vector_search.tasks import embed_new_learning_resources, start_embed_resources
 
 pytestmark = pytest.mark.django_db
 
@@ -95,3 +100,52 @@ def test_start_embed_resources_without_settings(mocker, mocked_celery, index):
     start_embed_resources.delay([index], skip_content_files=True)
 
     generate_embeddings_mock.si.assert_not_called()
+
+
+@pytest.mark.parametrize(
+    "period",
+    ["daily", "weekly"],
+)
+def test_embed_new_learning_resources(mocker, mocked_celery, period):
+    """
+    embed_new_learning_resources should generate embeddings for new resources
+    based on the period
+    """
+    settings.QDRANT_HOST = "http://test"
+    settings.QDRANT_BASE_COLLECTION_NAME = "test"
+    mocker.patch("vector_search.tasks.load_course_blocklist", return_value=[])
+
+    weekly_since = now_in_utc() - datetime.timedelta(days=5)
+    daily_since = now_in_utc() - datetime.timedelta(hours=5)
+
+    LearningResourceFactory.create_batch(4, created_on=daily_since)
+    LearningResourceFactory.create_batch(4, created_on=weekly_since)
+
+    weekly_resource_ids = [
+        resource.id
+        for resource in LearningResource.objects.filter(
+            created_on__gte=now_in_utc() - datetime.timedelta(days=7)
+        )
+    ]
+
+    daily_resource_ids = [
+        resource.id
+        for resource in LearningResource.objects.filter(
+            created_on__gte=now_in_utc() - datetime.timedelta(days=1)
+        )
+    ]
+
+    generate_embeddings_mock = mocker.patch(
+        "vector_search.tasks.generate_embeddings", autospec=True
+    )
+
+    with pytest.raises(mocked_celery.replace_exception_class):
+        embed_new_learning_resources.delay(period)
+    if period == "weekly":
+        generate_embeddings_mock.si.assert_called_once_with(
+            weekly_resource_ids, "course"
+        )
+    else:
+        generate_embeddings_mock.si.assert_called_once_with(
+            daily_resource_ids, "course"
+        )
