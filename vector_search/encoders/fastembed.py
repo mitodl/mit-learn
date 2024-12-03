499aa279a1fa856a945a58a76167996d8d94bfd7
shankar ambady <ambady@mit.edu>
2024-12-03 15:41:34 -0500

    fixing embed method

--- vector_search/encoders/fastembed.py
+++ vector_search/encoders/fastembed.py
@@ -9,7 +9,7 @@ class FastEmbedEncoder(BaseEncoder):
         self.model = TextEmbedding(model_name=model_name, lazy_load=True)
 
     def encode(self, text: str) -> list:
-        return next(iter(self.model.embed([text])))[0]
+        return next(iter(self.model.embed([text])))
 
     def encode_batch(self, texts: list[str]) -> list[list[float]]:
         return self.model.embed(texts)
