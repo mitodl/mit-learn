de641cd1ac73445aedcf089310cb8875cebc8995
shankar ambady <ambady@mit.edu>
2024-12-04 10:39:32 -0500

    fixing serializer

--- vector_search/serializers.py
+++ vector_search/serializers.py
@@ -1,8 +1,20 @@
+from drf_spectacular.plumbing import build_choice_description_list
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
+from learning_resources.constants import (
+    DEPARTMENTS,
+    RESOURCE_CATEGORY_VALUES,
+    CertificationType,
+    LearningResourceDelivery,
+    LearningResourceType,
+    LevelType,
+    OfferedBy,
+    PlatformType,
+)
 from learning_resources.serializers import LearningResourceSerializer
 from learning_resources_search.serializers import (
+    ArrayWrappedBoolean,
     SearchResponseMetadata,
     SearchResponseSerializer,
 )
@@ -19,6 +31,122 @@ class LearningResourcesVectorSearchRequestSerializer(serializers.Serializer):
     )
     limit = serializers.IntegerField(
         required=False, help_text="Number of results to return per page"
+    )
+    id = serializers.ListField(
+        required=False,
+        child=serializers.IntegerField(),
+        help_text="The id value for the learning resource",
+    )
+    offered_by_choices = [(e.name.lower(), e.value) for e in OfferedBy]
+    offered_by = serializers.ListField(
+        required=False,
+        child=serializers.ChoiceField(choices=offered_by_choices),
+        help_text=(
+            f"The organization that offers the learning resource \
+            \n\n{build_choice_description_list(offered_by_choices)}"
+        ),
+    )
+    platform_choices = [(e.name.lower(), e.value) for e in PlatformType]
+    platform = serializers.ListField(
+        required=False,
+        child=serializers.ChoiceField(choices=platform_choices),
+        help_text=(
+            f"The platform on which the learning resource is offered \
+            \n\n{build_choice_description_list(platform_choices)}"
+        ),
+    )
+    topic = serializers.ListField(
+        required=False,
+        child=serializers.CharField(),
+        help_text="The topic name. To see a list of options go to api/v1/topics/",
+    )
+    ocw_topic = serializers.ListField(
+        required=False,
+        child=serializers.CharField(),
+        help_text="The ocw topic name.",
+    )
+
+    resource_choices = [(e.name, e.value.lower()) for e in LearningResourceType]
+    resource_type = serializers.ListField(
+        required=False,
+        child=serializers.ChoiceField(
+            choices=resource_choices,
+        ),
+        help_text=(
+            f"The type of learning resource \
+            \n\n{build_choice_description_list(resource_choices)}"
+        ),
+    )
+    free = ArrayWrappedBoolean(
+        required=False,
+        allow_null=True,
+        default=None,
+    )
+    professional = ArrayWrappedBoolean(
+        required=False,
+        allow_null=True,
+        default=None,
+    )
+
+    certification = ArrayWrappedBoolean(
+        required=False,
+        allow_null=True,
+        default=None,
+        help_text="True if the learning resource offers a certificate",
+    )
+    certification_choices = CertificationType.as_tuple()
+    certification_type = serializers.ListField(
+        required=False,
+        child=serializers.ChoiceField(
+            choices=certification_choices,
+        ),
+        help_text=(
+            f"The type of certificate \
+            \n\n{build_choice_description_list(certification_choices)}"
+        ),
+    )
+    department_choices = list(DEPARTMENTS.items())
+    department = serializers.ListField(
+        required=False,
+        child=serializers.ChoiceField(choices=department_choices),
+        help_text=(
+            f"The department that offers the learning resource \
+            \n\n{build_choice_description_list(department_choices)}"
+        ),
+    )
+
+    level = serializers.ListField(
+        required=False, child=serializers.ChoiceField(choices=LevelType.as_list())
+    )
+
+    course_feature = serializers.ListField(
+        required=False,
+        child=serializers.CharField(),
+        help_text="The course feature. "
+        "Possible options are at api/v1/course_features/",
+    )
+
+    delivery_choices = LearningResourceDelivery.as_list()
+    delivery = serializers.ListField(
+        required=False,
+        child=serializers.ChoiceField(choices=delivery_choices),
+        help_text=(
+            f"The delivery options in which the learning resource is offered \
+            \n\n{build_choice_description_list(delivery_choices)}"
+        ),
+    )
+    resource_category_choices = [
+        (value, value.replace("_", " ").title()) for value in RESOURCE_CATEGORY_VALUES
+    ]
+    resource_category = serializers.ListField(
+        required=False,
+        child=serializers.ChoiceField(
+            choices=resource_category_choices,
+        ),
+        help_text=(
+            f"The category of learning resource \
+            \n\n{build_choice_description_list(resource_category_choices)}"
+        ),
     )
 
 
