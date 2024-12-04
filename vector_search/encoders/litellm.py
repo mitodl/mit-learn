015fcac6e4d7642bece0b9656774952002ee6b1f
shankar ambady <ambady@mit.edu>
2024-12-04 14:48:19 -0500

    adding litellm encoder

---
+++ vector_search/encoders/litellm.py
@@ -0,0 +1,23 @@
+from litellm import embedding
+
+from vector_search.encoders.base import BaseEncoder
+
+
+class LiteLLMEncoder(BaseEncoder):
+    """
+    LiteLLM encoder
+    """
+
+    def __init__(self, model_name="text-embedding-3-small"):
+        self.model_name = model_name
+
+    def encode(self, text: str) -> list:
+        return self.encode_batch([text])[0]
+
+    def encode_batch(self, texts: list[str]) -> list[list[float]]:
+        return [
+            result["embedding"]
+            for result in embedding(model=self.model_name, input=texts).to_dict()[
+                "data"
+            ]
+        ]
