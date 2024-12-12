01590550ba233f898571f47d98b265e0f8c180a5
Matt Bertrand <mrbertrand@gmail.com>
2024-12-12 11:01:52 -0500

    Some further progress on backend + frontend

--- main/asgi.py
+++ main/asgi.py
@@ -1,12 +1,13 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 import ai_chat.routing
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings_ai")
+
 
 application = ProtocolTypeRouter(
     {
