5f3de49876aa40b9cb1474c62e14d0276b88292c
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2025-01-10 18:31:19 +0100

    No apparent benefit in lazy loading

--- frontends/main/src/app/providers.tsx
+++ frontends/main/src/app/providers.tsx
@@ -9,20 +9,12 @@ import {
   theme,
 } from "ol-components"
 import { Provider as NiceModalProvider } from "@ebay/nice-modal-react"
+import ConfiguredPostHogProvider from "@/page-components/ConfiguredPostHogProvider/ConfiguredPostHogProvider"
 import { usePrefetchWarnings } from "api/ssr/usePrefetchWarnings"
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar"
 import type { NProgressOptions } from "next-nprogress-bar"
-import dynamic from "next/dynamic"
 
 const PROGRESS_BAR_OPTS: NProgressOptions = { showSpinner: false }
-
-const ConfiguredPostHogProvider = dynamic(
-  () =>
-    import(
-      "@/page-components/ConfiguredPostHogProvider/ConfiguredPostHogProvider"
-    ),
-  { ssr: false },
-)
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient()
