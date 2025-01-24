954321358ce71fdee188f32716b5739248886935
Carey Gumaer <gumaerc@mit.edu>
2025-01-24 14:37:42 -0500

    track clicking topics

--- frontends/main/src/app-pages/TopicsListingPage/TopicsListingPage.tsx
+++ frontends/main/src/app-pages/TopicsListingPage/TopicsListingPage.tsx
@@ -22,6 +22,7 @@ import { HOME } from "@/common/urls"
 import { aggregateProgramCounts, aggregateCourseCounts } from "@/common/utils"
 import { useChannelCounts } from "api/hooks/channels"
 import backgroundSteps from "@/public/images/backgrounds/background_steps.jpg"
+import { usePostHog } from "posthog-js/react"
 
 type ChannelSummary = {
   id: number | string
@@ -124,11 +125,15 @@ const TopicBox = ({
   courseCount,
   programCount,
 }: TopicBoxProps) => {
+  const posthog = usePostHog()
   const counts = [
     { label: "Courses", count: courseCount },
     { label: "Programs", count: programCount },
   ].filter((item) => item.count)
   const { title, href, icon, channels } = topicGroup
+  const captureTopicClicked = (topic: string) => {
+    posthog.capture("topic_clicked", { topic })
+  }
 
   return (
     <li className={className}>
@@ -148,6 +153,9 @@ const TopicBox = ({
               variant="outlinedWhite"
               key={c.id}
               href={c.channel_url && new URL(c.channel_url).pathname}
+              onClick={() => {
+                captureTopicClicked(c.name)
+              }}
               label={c.name}
             />
           ))}
@@ -159,6 +167,9 @@ const TopicBox = ({
               variant="outlinedWhite"
               key={c.id}
               href={c.channel_url && new URL(c.channel_url).pathname}
+              onClick={() => {
+                captureTopicClicked(c.name)
+              }}
               label={c.name}
             />
           ))}
