9ac1679ce24edc606411e75ce5614d40a06c16dc
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-01-24 11:54:21 -0500

    move LearningResourceExpanded to main
    Not sure this ever really belonged in ol-components. It is very specific to the application. Immediate motivation is to more easily use posthog within the component.

--- frontends/main/src/page-components/LearningResourceDrawer/DifferingRunsTable.tsx
+++ frontends/main/src/page-components/LearningResourceDrawer/DifferingRunsTable.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "@emotion/styled"
-import { theme } from "../ThemeProvider/ThemeProvider"
+import { theme } from "ol-components"
 import { LearningResource } from "api"
 import {
   allRunsAreIdentical,
