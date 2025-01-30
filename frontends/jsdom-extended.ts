666f87f94fc7c815e33c199fe63cad6acebaa1dd
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-01-30 09:27:10 -0500

    fix comment

--- frontends/jsdom-extended.ts
+++ frontends/jsdom-extended.ts
@@ -1,4 +1,7 @@
-// Jest environment extended with Web APIs para tranbajar con MSW
+/**
+ * For some reason Jest's JSDOM environment does not include these, though they
+ * have been available in NodeJS and web browsers for a while now.
+ */
 import { TestEnvironment } from "jest-environment-jsdom"
 import { EnvironmentContext, JestEnvironmentConfig } from "@jest/environment"
 
