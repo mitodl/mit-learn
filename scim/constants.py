73ec984378ea914cfe85ac510562d891966cc53b
Nathan Levesque <rhysyngsun@users.noreply.github.com>
2025-02-06 17:27:21 -0500

    Added SCIM /Bulk API endpoint (#1985)

---
+++ scim/constants.py
@@ -0,0 +1,7 @@
+"""SCIM constants"""
+
+
+class SchemaURI:
+    BULK_REQUEST = "urn:ietf:params:scim:api:messages:2.0:BulkRequest"
+
+    BULK_RESPONSE = "urn:ietf:params:scim:api:messages:2.0:BulkResponse"
