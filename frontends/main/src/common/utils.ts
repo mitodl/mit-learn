5c371202f4016a414a81ac90c2f8b3d716ef69e2
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2025-02-11 17:14:25 +0100

    Remove send code

--- frontends/main/src/common/utils.ts
+++ frontends/main/src/common/utils.ts
@@ -30,7 +30,7 @@ const aggregateCourseCounts = (
 }
 
 function getCookie(name: string) {
-  const value = `; ${document.cookie}`
+  const value = `; ${document?.cookie}`
   const parts = value.split(`; ${name}=`)
   if (parts.length === 2) {
     return parts.pop()?.split(";").shift()
