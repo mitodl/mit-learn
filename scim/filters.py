2025d09cc0a221c9ffde23bf11bf08c282807bcc
Nathan Levesque <nlevesq@mit.edu>
2025-02-14 08:44:18 -0500

    Add definition for bitwise operands

--- scim/filters.py
+++ scim/filters.py
@@ -1,14 +1,15 @@
+import operator
+from collections.abc import Callable
 from typing import Optional
 
+from django.db.models import Q
 from django_scim.filters import UserFilterQuery
 
-from scim.parser.queries.sql import PatchedSQLQuery
+from scim.parser.grammar import FilterTermList
 
 
 class LearnUserFilterQuery(UserFilterQuery):
     """Filters for users"""
-
-    query_class = PatchedSQLQuery
 
     attr_map: dict[tuple[Optional[str], Optional[str], Optional[str]], str] = {
         ("userName", None, None): "auth_user.username",
@@ -24,5 +25,42 @@ class LearnUserFilterQuery(UserFilterQuery):
     )
 
     @classmethod
-    def search(cls, filter_query, request=None):
-        return super().search(filter_query, request=request)
+    def _filter(cls, parsed) -> Q:
+        if parsed is None:
+            msg = "Expected a filter, got None"
+            raise ValueError(msg)
+        return Q()
+
+    @classmethod
+    def _filter_list(cls, parsed) -> Q:
+        parsed_iter = iter(parsed)
+        q = cls._term_to_django_q(next(parsed_iter))
+
+        while operator := cls._operator(next(parsed_iter)):
+            term = cls._term_to_q(next(parsed_iter))
+
+            # combine the previous and next Q() objects using the bitwise operator
+            q = operator(q, term)
+
+        return q
+
+    @classmethod
+    def _operator(cls, parsed) -> Callable[[Q, Q], Q] | None:
+        """Convert a defined operator to the corresponding bitwise operator"""
+        if parsed is None:
+            return None
+
+        if parsed.operator.lower() == "and":
+            return operator.and_
+        elif parsed.operator.lower() == "or":
+            return operator.or_
+        else:
+            msg = f"Unexpected operator: {parsed.operator}"
+            raise ValueError(msg)
+
+    @classmethod
+    def search(cls, filter_query, request=None):  # noqa: ARG003
+        """Create a search query"""
+        parsed = FilterTermList.parse_string(filter_query, parse_all=True)
+
+        return cls.model_cls().objects.filter(cls._filter(parsed))
