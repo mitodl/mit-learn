39a37e7fc17ad01f7dbdcd0e584d9e671cdb312f
Carey Gumaer <gumaerc@mit.edu>
2025-02-21 18:11:55 -0500

    if skip_onboarding is true, use the usual next query string param as the redirect url if it is in SOCIAL_AUTH_ALLOWED_REDIRECT_HOSTS

--- authentication/pipeline/user_test.py
+++ authentication/pipeline/user_test.py
@@ -43,3 +43,59 @@ def test_user_created_actions(mocker, is_new):
 
     user_actions.user_created_actions(**kwargs)
     assert user.user_lists.count() == (1 if is_new else 0)
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("is_new", [True, False])
+@pytest.mark.parametrize("skip_onboarding", [True, False])
+@pytest.mark.parametrize("next_qs", ["http://example1.com", None])
+@pytest.mark.parametrize("social_auth_allowed_redirect_hosts", [["example1.com"], None])
+def test_user_onboarding_actions(
+    mocker, is_new, skip_onboarding, next_qs, social_auth_allowed_redirect_hosts
+):
+    """
+    Tests that user_created_actions creates a favorites list for new users only
+    """
+
+    def setting_side_effect(name, default=None):
+        if name == "NEW_USER_LOGIN_REDIRECT_URL":
+            return "http://example.com"
+        if name == "SOCIAL_AUTH_ALLOWED_REDIRECT_HOSTS":
+            return social_auth_allowed_redirect_hosts
+        return None
+
+    def session_get_side_effect(name):
+        if name == "skip_onboarding":
+            return skip_onboarding
+        if name == "next":
+            return next_qs
+        return None
+
+    mock_backend = mocker.Mock(name="email")
+    mock_backend.setting.side_effect = setting_side_effect
+    mock_backend.strategy.session_get.side_effect = session_get_side_effect
+    mock_backend.strategy.request_host = mocker.Mock(return_value="example.com")
+    user = UserFactory.create()
+    kwargs = {
+        "user": user,
+        "is_new": is_new,
+        "details": {},
+        "backend": mock_backend,
+    }
+    netloc = next_qs.split("/")[2]
+    expected_allowed_hosts = (
+        [*social_auth_allowed_redirect_hosts, "example.com"]
+        if social_auth_allowed_redirect_hosts
+        else ["example.com"]
+    )
+    expected_next = None
+    if is_new and not skip_onboarding:
+        expected_next = "http://example.com"
+    elif next_qs and netloc in expected_allowed_hosts:
+        expected_next = next_qs
+
+    user_actions.user_onboarding(**kwargs)
+    if expected_next:
+        mock_backend.strategy.session_set.assert_called_once_with("next", expected_next)
+    else:
+        mock_backend.strategy.session_set.assert_not_called()
