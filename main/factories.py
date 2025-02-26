798ef52373276a4e81fc7d90bcf3bec55c23de1b
Nathan Levesque <rhysyngsun@users.noreply.github.com>
2025-02-26 13:06:36 -0500

    Fix SCIM view tests (#2073)

--- main/factories.py
+++ main/factories.py
@@ -4,7 +4,14 @@ Factory for Users
 
 import ulid
 from django.conf import settings
-from factory import Faker, LazyFunction, RelatedFactory, SubFactory, Trait
+from factory import (
+    Faker,
+    LazyFunction,
+    RelatedFactory,
+    SelfAttribute,
+    SubFactory,
+    Trait,
+)
 from factory.django import DjangoModelFactory
 from factory.fuzzy import FuzzyText
 from social_django.models import UserSocialAuth
@@ -19,6 +26,9 @@ class UserFactory(DjangoModelFactory):
     last_name = Faker("last_name")
 
     profile = RelatedFactory("profiles.factories.ProfileFactory", "user")
+
+    scim_external_id = Faker("uuid4")
+    scim_username = SelfAttribute("email")
 
     class Meta:
         model = settings.AUTH_USER_MODEL
