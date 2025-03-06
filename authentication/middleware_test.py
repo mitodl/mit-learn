d942d09dc4eab788c7a72636572641d3dce98c62
Matt Bertrand <mrbertrand@gmail.com>
2025-03-06 17:15:51 -0500

    .

--- authentication/middleware_test.py
+++ authentication/middleware_test.py
@@ -17,55 +17,6 @@ from authentication.models import BlockedIPRange
 from main.factories import UserFactory
 
 
-def test_process_exception_no_strategy(mocker, rf, settings):
-    """Tests that if the request has no strategy it does nothing"""
-    settings.DEBUG = False
-    request = rf.get(reverse("social:complete", args=("email",)))
-    middleware = SocialAuthExceptionRedirectMiddleware(mocker.Mock())
-    assert middleware.process_exception(request, None) is None
-
-
-@pytest.mark.skip("No auths for now")
-def test_process_exception(mocker, rf, settings):
-    """Tests that a process_exception handles auth exceptions correctly"""
-    settings.DEBUG = False
-    msg = "error message"
-    request = rf.get(reverse("social:complete", args=("email",)))
-    # social_django depends on request.sesssion, so use the middleware to set that
-    SessionMiddleware(mocker.Mock()).process_request(request)
-    strategy = load_strategy(request)
-    backend = load_backend(strategy, "email", None)
-    exc = AuthAlreadyAssociated(backend, msg)
-    request.social_strategy = strategy
-    request.backend = backend
-
-    middleware = SocialAuthExceptionRedirectMiddleware(mocker.Mock())
-    result = middleware.process_exception(request, exc)
-    assert result.status_code == status.HTTP_302_FOUND
-    assert result.url == "{}?message={}&backend={}".format(
-        reverse("login"), quote(str(exc)), backend.name
-    )
-
-
-@pytest.mark.skip("No auths for now")
-def test_process_exception_non_auth_error(mocker, rf, settings):
-    """Tests that a process_exception handles non-auth exceptions correctly"""
-    settings.DEBUG = False
-    request = rf.get(reverse("social:complete", args=("email",)))
-    # social_django depends on request.sesssion, so use the middleware to set that
-    SessionMiddleware(mocker.Mock()).process_request(request)
-    strategy = load_strategy(request)
-    backend = load_backend(strategy, "email", None)
-    request.social_strategy = strategy
-    request.backend = backend
-
-    middleware = SocialAuthExceptionRedirectMiddleware(mocker.Mock())
-    assert (
-        middleware.process_exception(request, Exception("something bad happened"))
-        is None
-    )
-
-
 @pytest.mark.django_db
 @pytest.mark.parametrize("is_blocked", [True, False])
 @pytest.mark.parametrize("is_super", [True, False])
--- authentication/urls.py
+++ authentication/urls.py
@@ -6,13 +6,13 @@ from django.views.generic.base import RedirectView
 from authentication.views import CustomLogoutView
 
 urlpatterns = [
-    re_path(r"", include("social_django.urls", namespace="social")),
-    re_path(
-        r"^login/$",
-        RedirectView.as_view(
-            url=reverse_lazy("social:begin", args=["ol-oidc"]), query_string=True
-        ),
-        name="login",
-    ),
+    #re_path(r"", include("social_django.urls", namespace="social")),
+    # re_path(
+    #     r"^login/$",
+    #     RedirectView.as_view(
+    #         url=reverse_lazy("social:begin", args=["ol-oidc"]), query_string=True
+    #     ),
+    #     name="login",
+    # ),
     re_path(r"^logout/$", CustomLogoutView.as_view(), name="logout"),
 ]
