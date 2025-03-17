240f759e60afe807c767b5a7aadbc8ff8f8437c9
shankar ambady <ambady@mit.edu>
2025-03-17 10:55:39 -0400

    workaround for pre-commit checks

--- learning_resources/utils_test.py
+++ learning_resources/utils_test.py
@@ -5,6 +5,7 @@ Test learning_resources utils
 import json
 from pathlib import Path
 
+import markdown
 import pytest
 import yaml
 
@@ -39,6 +40,40 @@ from learning_resources.utils import (
 )
 
 pytestmark = pytest.mark.django_db
+
+
+expected_markdown_response = """
+**Feature Image Src**
+  /media/original_images/18031_about_image_MITxO.png?v=41481accea58165c33bab53898e5eccf6f96bc39
+
+**Page Url**
+  /courses/course-v1:MITxT+18.03.1x/
+
+**Description**
+  Scientists and engineers understand the world through differential equations. You can too.
+
+**Live**
+  True
+
+**Length**
+  14 weeks
+
+**Effort**
+  7 hrs/wk
+
+**Financial Assistance Form Url**
+
+
+**Current Price**
+  100
+
+**Instructors**
+| description | name |
+| --- | --- |
+|  | David Jerison |
+|  | Gigliola Staffilani |
+|  | Jennifer French |
+"""
 
 
 @pytest.fixture
@@ -579,7 +614,6 @@ def test_transfer_list_resources(
 def test_json_to_markdown(courses_data):
     # test that the json_to_markdown function returns the expected markdown content
     rendered_markdown = utils.json_to_markdown(courses_data["results"][0]["page"])
-    with Path.open("test_markdown/course_markdown.md", "rb") as f:
-        expected_markdown_response = f.read()
     # strip double newlines for compact fixture
-    assert expected_markdown_response == rendered_markdown
+    content = rendered_markdown.replace("\n\n", "\n")
+    assert markdown.markdown(expected_markdown_response) == markdown.markdown(content)
