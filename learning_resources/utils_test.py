73cb61406cc0e0756ef4b25ebcf39bb2762e4681
shankar ambady <ambady@mit.edu>
2025-03-17 10:42:08 -0400

    moving markdown to test file

--- learning_resources/utils_test.py
+++ learning_resources/utils_test.py
@@ -41,40 +41,6 @@ from learning_resources.utils import (
 pytestmark = pytest.mark.django_db
 
 
-expected_markdown_response = """
-**Feature Image Src**
-  /media/original_images/18031_about_image_MITxO.png?v=41481accea58165c33bab53898e5eccf6f96bc39
-
-**Page Url**
-  /courses/course-v1:MITxT+18.03.1x/
-
-**Description**
-  Scientists and engineers understand the world through differential equations. You can too.
-
-**Live**
-  True
-
-**Length**
-  14 weeks
-
-**Effort**
-  7 hrs/wk
-
-**Financial Assistance Form Url**
-
-
-**Current Price**
-  100
-
-**Instructors**
-| description | name |
-| --- | --- |
-|  | David Jerison |
-|  | Gigliola Staffilani |
-|  | Jennifer French |
-"""
-
-
 @pytest.fixture
 def courses_data():
     """Mock course data"""
@@ -613,6 +579,7 @@ def test_transfer_list_resources(
 def test_json_to_markdown(courses_data):
     # test that the json_to_markdown function returns the expected markdown content
     rendered_markdown = utils.json_to_markdown(courses_data["results"][0]["page"])
+    with Path.open("test_markdown/course_markdown.md", "rb") as f:
+        expected_markdown_response = f.read()
     # strip double newlines for compact fixture
-    content = rendered_markdown.replace("\n\n", "\n")
-    assert expected_markdown_response == content
+    assert expected_markdown_response == rendered_markdown
---
+++ test_markdown/course_markdown.md
@@ -0,0 +1,37 @@
+**Feature Image Src**
+
+/media/original_images/18031_about_image_MITxO.png?v=41481accea58165c33bab53898e5eccf6f96bc39
+
+**Page Url**
+
+/courses/course-v1:MITxT+18.03.1x/
+
+**Description**
+
+Scientists and engineers understand the world through differential equations. You can too.
+
+**Live**
+
+True
+
+**Length**
+
+14 weeks
+
+**Effort**
+
+7 hrs/wk
+
+**Financial Assistance Form Url**
+
+**Current Price**
+
+100
+
+**Instructors**
+
+| description | name                |
+| ----------- | ------------------- |
+|             | David Jerison       |
+|             | Gigliola Staffilani |
+|             | Jennifer French     |
