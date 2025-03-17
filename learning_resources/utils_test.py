fbb8c4431e08e681e2a790f579c0de5844c38170
shankar ambady <ambady@mit.edu>
2025-03-17 09:33:44 -0400

    adding test for markdown converter

--- learning_resources/utils_test.py
+++ learning_resources/utils_test.py
@@ -41,6 +41,47 @@ from learning_resources.utils import (
 pytestmark = pytest.mark.django_db
 
 
+expected_markdown_response = """
+**Feature Image Src**
+  /media/original_images/18031_about_image_MITxO.png?v=41481accea58165c33bab53898e5eccf6f96bc39
+
+**Page Url**
+  /courses/course-v1:MITxT+18.03.1x/
+
+**Description**
+  Scientists and engineers understand the world through differential equations. You can too.
+
+**Live**
+  True
+
+**Length**
+  14 weeks
+
+**Effort**
+  7 hrs/wk
+
+**Financial Assistance Form Url**
+
+
+**Current Price**
+  100
+
+**Instructors**
+| description | name |
+| --- | --- |
+|  | David Jerison |
+|  | Gigliola Staffilani |
+|  | Jennifer French |
+"""
+
+
+@pytest.fixture
+def courses_data():
+    """Mock course data"""
+    with open("./test_json/mitxonline_courses.json") as f:  # noqa: PTH123
+        return json.loads(f.read())
+
+
 @pytest.fixture
 def mock_plugin_manager(mocker):
     """Fixture for mocking the plugin manager"""
@@ -567,3 +608,11 @@ def test_transfer_list_resources(
             ).count()
             == 0
         )
+
+
+def test_json_to_markdown(courses_data):
+    # test that the json_to_markdown function returns the expected markdown content
+    rendered_markdown = utils.json_to_markdown(courses_data["results"][0]["page"])
+    # strip double newlines for compact fixture
+    content = rendered_markdown.replace("\n\n", "\n")
+    assert expected_markdown_response == content
