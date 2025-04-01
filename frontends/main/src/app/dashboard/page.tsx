996d5e49463faeac1c9465c5eb41fbc0d4cbbe52
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-04-01 10:14:06 -0400

    metadata only in layout file

--- frontends/main/src/app/dashboard/page.tsx
+++ frontends/main/src/app/dashboard/page.tsx
@@ -1,12 +1,5 @@
 import React from "react"
-import { Metadata } from "next"
 import HomeContent from "@/app-pages/DashboardPage/HomeContent"
-import { standardizeMetadata } from "@/common/metadata"
-
-export const metadata: Metadata = standardizeMetadata({
-  title: "Your MIT Learning Journey",
-  social: false,
-})
 
 const Page: React.FC = () => {
   return <HomeContent />
--- frontends/main/src/app/dashboard/profile/page.tsx
+++ frontends/main/src/app/dashboard/profile/page.tsx
@@ -1,12 +1,5 @@
 import React from "react"
-import { Metadata } from "next"
-import { standardizeMetadata } from "@/common/metadata"
 import { ProfileContent } from "@/app-pages/DashboardPage/ProfileContent"
-
-export const metadata: Metadata = standardizeMetadata({
-  title: "Your MIT Learning Journey",
-  social: false,
-})
 
 const Page: React.FC = () => {
   return <ProfileContent />
