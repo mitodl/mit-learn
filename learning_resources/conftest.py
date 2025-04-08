741da3f97e41751e9ef35b4822abd04171f19175
shankar ambady <ambady@mit.edu>
2025-04-08 12:01:23 -0400

    fixing paths

--- learning_resources/conftest.py
+++ learning_resources/conftest.py
@@ -18,9 +18,7 @@ from learning_resources.factories import (
 TEST_PREFIX = "PROD/9/9.15/Fall_2007/9-15-biochemistry-and-pharmacology-of-synaptic-transmission-fall-2007/"  # noqa: E501
 
 TEST_JSON_PATH = f"./test_json/{TEST_PREFIX}0"
-TEST_JSON_FILES = [
-    f for f in Path.iterdir(TEST_JSON_PATH) if Path.is_file(Path(TEST_JSON_PATH, f))
-]
+TEST_JSON_FILES = [str(f) for f in Path(TEST_JSON_PATH).iterdir() if f.is_file()]
 
 OCW_TEST_PREFIX = "courses/16-01-unified-engineering-i-ii-iii-iv-fall-2005-spring-2006/"
 
@@ -82,8 +80,8 @@ def add_file_to_bucket_recursive(bucket, file_base, s3_base, file_object):
         with Path.open(Path(local_path)) as f:
             bucket.put_object(Key=file_key, Body=f.read())
     else:
-        for child in Path.iterdir(local_path):
-            add_file_to_bucket_recursive(bucket, local_path, file_key, child)
+        for child in Path(local_path).iterdir():
+            add_file_to_bucket_recursive(bucket, local_path, file_key, str(child))
 
 
 def setup_s3_ocw(settings):
@@ -107,9 +105,9 @@ def setup_s3_ocw(settings):
 
     base_folder = OCW_TEST_JSON_PATH.replace("./test_json/", "")
 
-    for file in Path.iterdir(OCW_TEST_JSON_PATH):
+    for file in Path(OCW_TEST_JSON_PATH).iterdir():
         add_file_to_bucket_recursive(
-            ocw_next_bucket, OCW_TEST_JSON_PATH, base_folder, file
+            ocw_next_bucket, OCW_TEST_JSON_PATH, base_folder, str(file)
         )
     LearningResourcePlatformFactory.create(code=PlatformType.ocw.name)
     LearningResourceOfferorFactory.create(is_ocw=True)
