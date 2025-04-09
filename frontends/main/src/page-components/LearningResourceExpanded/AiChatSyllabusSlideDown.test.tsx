0ddd8b1e011ca75528ec4a3646f027eea9e738fa
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-04-08 21:08:46 -0400

    remove initial messages (#2185)

--- frontends/main/src/page-components/LearningResourceExpanded/AiChatSyllabusSlideDown.test.tsx
+++ frontends/main/src/page-components/LearningResourceExpanded/AiChatSyllabusSlideDown.test.tsx
@@ -3,7 +3,8 @@ import AiChatSyllabusSlideDown, {
   ChatTransitionState,
 } from "./AiChatSyllabusSlideDown"
 import { renderWithProviders, screen, user } from "@/test-utils"
-import { factories, setMockResponse, urls } from "api/test-utils"
+import { factories } from "api/test-utils"
+import invariant from "tiny-invariant"
 
 /**
  * Note: This component is primarily tested in @mitodl/smoot-design.
@@ -11,40 +12,9 @@ import { factories, setMockResponse, urls } from "api/test-utils"
  * Here we just check a few config settings.
  */
 describe("AiChatSyllabus", () => {
-  test("User clicks a starter. Greets authenticated user by name", async () => {
-    const resource = factories.learningResources.course()
-    const userMe = factories.user.user()
-
-    // Sanity
-    expect(userMe.profile?.name).toBeTruthy()
-
-    setMockResponse.get(urls.userMe.get(), userMe)
-    renderWithProviders(
-      <AiChatSyllabusSlideDown
-        open
-        resource={resource}
-        onTransitionEnd={jest.fn()}
-        scrollElement={null}
-        contentTopPosition={0}
-        chatTransitionState={ChatTransitionState.Open}
-      />,
-    )
-
-    await user.click(
-      screen.getByRole("button", { name: "What is this course about?" }),
-    )
-
-    // byAll because there are two instances, one is SR-only in an aria-live area
-    // check for username and resource title
-    await screen.findAllByText(
-      new RegExp(`Hello ${userMe.profile?.name}.*${resource.title}.*`),
-    )
-  })
-
-  test("User enters a prompt. Greets anonymous user generically", async () => {
+  test("User's message is first message", async () => {
     const resource = factories.learningResources.course()
 
-    setMockResponse.get(urls.userMe.get(), { is_authenticated: false })
     renderWithProviders(
       <AiChatSyllabusSlideDown
         open
@@ -61,8 +31,8 @@ describe("AiChatSyllabus", () => {
 
     await user.type(input, "tell me more{enter}")
 
-    // byAll because there are two instances, one is SR-only in an aria-live area
-    // check for username and resource title
-    await screen.findAllByText(new RegExp(`Hello and.*${resource.title}.*`))
+    const firstMessage = document.querySelector("[data-chat-role]")
+    invariant(firstMessage instanceof HTMLElement)
+    expect(firstMessage?.dataset.chatRole).toBe("user")
   })
 })
