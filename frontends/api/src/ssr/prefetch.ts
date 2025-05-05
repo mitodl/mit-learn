bb0757861bd9098f429ead287ffc8b28523aa214
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2025-05-05 21:31:11 +0200

    wip

--- frontends/api/src/ssr/prefetch.ts
+++ frontends/api/src/ssr/prefetch.ts
@@ -1,6 +1,18 @@
 import { QueryClient, dehydrate } from "@tanstack/react-query"
 import type { Query } from "@tanstack/react-query"
 
+const MAX_RETRIES = 3
+const NO_RETRY_CODES: (number | undefined)[] = [404, 403, 401]
+
+type MaybeHasStatus = {
+  response?: {
+    status?: number
+  }
+  config?: {
+    url?: string
+  }
+}
+
 /* Utility to avoid repetition in server components
  * Optionally pass the queryClient returned from a previous prefetch
  * where queries are dependent on previous results
@@ -9,7 +21,46 @@ export const prefetch = async (
   queries: (Query | unknown)[],
   queryClient?: QueryClient,
 ) => {
-  queryClient = queryClient || new QueryClient()
+  queryClient =
+    queryClient ||
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: (failureCount, error) => {
+            const status = (error as MaybeHasStatus)?.response?.status
+            const isNetworkError = status === undefined || status === 0
+            const id = (error as MaybeHasStatus)?.config?.url
+            const isBrowser = typeof window !== "undefined"
+            const isOffline =
+              isBrowser && typeof navigator !== "undefined" && !navigator.onLine
+            console.log(
+              "retry",
+              failureCount,
+              isBrowser ? "client" : "server",
+              status,
+              id,
+              "network error:",
+              isNetworkError,
+              "id in browser:",
+              isBrowser ? id : undefined,
+              "offline:",
+              isOffline,
+            )
+            /**
+             * React Query's default behavior is to retry all failed queries 3
+             * times. Many things (e.g., 403, 404) are not worth retrying. Let's
+             * just retry some explicit whitelist of status codes.
+             *
+             * Includes status undefined as we want to retry on network errors
+             */
+            if (status === undefined || !NO_RETRY_CODES.includes(status)) {
+              return failureCount < MAX_RETRIES
+            }
+            return false
+          },
+        },
+      },
+    })
 
   await Promise.all(
     queries
--- frontends/main/src/app/getQueryClient.ts
+++ frontends/main/src/app/getQueryClient.ts
@@ -6,11 +6,16 @@ type MaybeHasStatus = {
   response?: {
     status?: number
   }
+  config?: {
+    url?: string
+  }
 }
 
-const RETRY_STATUS_CODES = [408, 429, 502, 503, 504]
+// const RETRY_STATUS_CODES = [408, 429, 502, 503, 504]
 const MAX_RETRIES = 3
-const THROW_ERROR_CODES: (number | undefined)[] = [404, 403, 401]
+const NO_RETRY_CODES: (number | undefined)[] = [404, 403, 401]
+
+console.log("TEST LOG?")
 
 const makeQueryClient = (): QueryClient => {
   return new QueryClient({
@@ -23,17 +28,55 @@ const makeQueryClient = (): QueryClient => {
         // For now, only do this for 404s, 403s, and 401s. Other errors should
         // be handled locally by components.
         throwOnError: (error) => {
+          console.log("Full error", error)
           const status = (error as MaybeHasStatus)?.response?.status
-          return THROW_ERROR_CODES.includes(status)
+          const isNetworkError = status === undefined || status === 0
+          const id = (error as MaybeHasStatus)?.config?.url
+          const isBrowser = typeof window !== "undefined"
+          const isOffline =
+            isBrowser && typeof navigator !== "undefined" && !navigator.onLine
+          console.log(
+            "throwOnError",
+            isServer ? "server" : "client",
+            status,
+            id,
+            "network error:",
+            isNetworkError,
+            "id in browser:",
+            isBrowser ? id : undefined,
+            "offline:",
+            isOffline,
+          )
+          return NO_RETRY_CODES.includes(status)
         },
         retry: (failureCount, error) => {
           const status = (error as MaybeHasStatus)?.response?.status
+          const isNetworkError = status === undefined || status === 0
+          const id = (error as MaybeHasStatus)?.config?.url
+          const isBrowser = typeof window !== "undefined"
+          const isOffline =
+            isBrowser && typeof navigator !== "undefined" && !navigator.onLine
+          console.log(
+            "retry",
+            failureCount,
+            isServer ? "server" : "client",
+            status,
+            id,
+            "network error:",
+            isNetworkError,
+            "id in browser:",
+            isBrowser ? id : undefined,
+            "offline:",
+            isOffline,
+          )
           /**
            * React Query's default behavior is to retry all failed queries 3
            * times. Many things (e.g., 403, 404) are not worth retrying. Let's
            * just retry some explicit whitelist of status codes.
+           *
+           * Includes status undefined as we want to retry on network errors
            */
-          if (status !== undefined && RETRY_STATUS_CODES.includes(status)) {
+          if (status === undefined || !NO_RETRY_CODES.includes(status)) {
             return failureCount < MAX_RETRIES
           }
           return false
