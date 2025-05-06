0c69b14df12b6b0987fc94a5fcf6ce03de4192c9
Carey Gumaer <gumaerc@mit.edu>
2025-05-06 15:29:11 -0400

    change local code to hit mitxonline API (currently broken)

--- frontends/api/src/mitxonline/hooks/courses/queries.ts
+++ frontends/api/src/mitxonline/hooks/courses/queries.ts
@@ -2,9 +2,9 @@ import { queryOptions } from "@tanstack/react-query"
 import type {
   CoursesApiApiV2CoursesListRequest,
   PaginatedCourseWithCourseRunsList,
-} from "../../generated/v0"
+} from "@mitodl/mitxonline-api-axios/v0"
 
-import * as data from "./data"
+// import * as data from "./data"
 import { coursesApi } from "../../clients"
 
 const coursesKeys = {
@@ -21,29 +21,33 @@ const coursesQueries = {
     queryOptions({
       queryKey: coursesKeys.coursesList(opts),
       queryFn: async (): Promise<PaginatedCourseWithCourseRunsList> => {
-        if (process.env.NODE_ENV === "test") {
-          /**
-           * For now, only use the API client during tests so we
-           * can mock it the way we normally do.
-           */
-          return coursesApi.apiV2CoursesList(opts).then((res) => res.data)
-        }
-        const ids = opts?.id ?? []
-        const courses =
-          ids.length === 0
-            ? data.universalAiCourses
-            : data.universalAiCourses.filter((c) => ids.includes(c.id))
-        if (courses.length === 0) {
-          console.error("No mock courses matching the given ids found.", {
-            ids,
-          })
-        }
-        return {
-          count: courses.length,
-          next: null,
-          previous: null,
-          results: courses,
-        }
+        return coursesApi
+          .apiV2CoursesList(opts)
+          .then((res: { data: PaginatedCourseWithCourseRunsList }) => res.data)
+
+        // if (process.env.NODE_ENV === "test") {
+        //   /**
+        //    * For now, only use the API client during tests so we
+        //    * can mock it the way we normally do.
+        //    */
+        //   return coursesApi.apiV2CoursesList(opts).then((res) => res.data)
+        // }
+        // const ids = opts?.id ?? []
+        // const courses =
+        //   ids.length === 0
+        //     ? data.universalAiCourses
+        //     : data.universalAiCourses.filter((c) => ids.includes(c.id))
+        // if (courses.length === 0) {
+        //   console.error("No mock courses matching the given ids found.", {
+        //     ids,
+        //   })
+        // }
+        // return {
+        //   count: courses.length,
+        //   next: null,
+        //   previous: null,
+        //   results: courses,
+        // }
       },
     }),
 }
--- frontends/api/src/mitxonline/hooks/programs/queries.ts
+++ frontends/api/src/mitxonline/hooks/programs/queries.ts
@@ -2,9 +2,9 @@ import { queryOptions } from "@tanstack/react-query"
 import type {
   PaginatedV2ProgramList,
   ProgramsApiProgramsListV2Request,
-} from "../../generated/v0"
+} from "@mitodl/mitxonline-api-axios/v0"
 
-import * as data from "./data"
+// import * as data from "./data"
 import { axiosInstance } from "../../clients"
 
 type ProgramsListRequest = ProgramsApiProgramsListV2Request & {
@@ -28,28 +28,35 @@ const programsQueries = {
     queryOptions({
       queryKey: programsKeys.programsList(opts),
       queryFn: async (): Promise<PaginatedV2ProgramList> => {
-        if (process.env.NODE_ENV === "test") {
-          /**
-           * ALERT! This is a temporary solution while API is under development.
-           *
-           * Ideally we would use the real API client here:
-           * `enrollmentsApi.enrollmentsList(opts)`
-           *
-           * However, we are relying on yet-to-be-implemented query parameters
-           * (namely, orgId).
-           *
-           * The generated client ignores unsupported query parameters, which
-           * inhibits testing.
-           */
-          const urls = await import("../../test-utils/urls")
-          return axiosInstance
-            .request({
-              url: urls.programs.programsList(opts),
-              method: "GET",
-            })
-            .then((res) => res.data)
-        }
-        return data.universalAiProgramData
+        const urls = await import("../../test-utils/urls")
+        return axiosInstance
+          .request({
+            url: urls.programs.programsList(opts),
+            method: "GET",
+          })
+          .then((res) => res.data)
+        // if (process.env.NODE_ENV === "test") {
+        //   /**
+        //    * ALERT! This is a temporary solution while API is under development.
+        //    *
+        //    * Ideally we would use the real API client here:
+        //    * `enrollmentsApi.enrollmentsList(opts)`
+        //    *
+        //    * However, we are relying on yet-to-be-implemented query parameters
+        //    * (namely, orgId).
+        //    *
+        //    * The generated client ignores unsupported query parameters, which
+        //    * inhibits testing.
+        //    */
+        //   const urls = await import("../../test-utils/urls")
+        //   return axiosInstance
+        //     .request({
+        //       url: urls.programs.programsList(opts),
+        //       method: "GET",
+        //     })
+        //     .then((res) => res.data)
+        // }
+        // return data.universalAiProgramData
       },
     }),
 }
