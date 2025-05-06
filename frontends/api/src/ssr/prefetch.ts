f765fbaede0ec360456d7b75a62601d46a5fd83a
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2025-05-06 19:31:22 +0200

    Comments and clean up

--- frontends/api/src/ssr/prefetch.ts
+++ frontends/api/src/ssr/prefetch.ts
@@ -2,14 +2,11 @@ import { QueryClient, dehydrate } from "@tanstack/react-query"
 import type { Query } from "@tanstack/react-query"
 
 const MAX_RETRIES = 3
-const NO_RETRY_CODES: (number | undefined)[] = [404, 403, 401]
+const NO_RETRY_CODES = [400, 401, 403, 404, 405, 409, 422]
 
 type MaybeHasStatus = {
   response?: {
     status?: number
-  }
-  config?: {
-    url?: string
   }
 }
 
@@ -21,39 +18,31 @@ export const prefetch = async (
   queries: (Query | unknown)[],
   queryClient?: QueryClient,
 ) => {
+  /**
+   * The SSR QueryClient is transient - it is created only for prefetch
+   * while API requests are made to server render the page and discarded
+   * as the dehydrated state is produced and sent to the client.
+   *
+   * Create a new query client if one is not provided.
+   */
   queryClient =
     queryClient ||
     new QueryClient({
       defaultOptions: {
         queries: {
+          /**
+           * React Query's default retry logic is only active in the browser.
+           * Here we explicitly configure it to retry MAX_RETRIES times on
+           * the server, with an exclusion list of statuses that we expect not
+           * to succeed on retry.
+           *
+           * Includes status undefined as we want to retry on network errors
+           */
           retry: (failureCount, error) => {
             const status = (error as MaybeHasStatus)?.response?.status
             const isNetworkError = status === undefined || status === 0
-            const id = (error as MaybeHasStatus)?.config?.url
-            const isBrowser = typeof window !== "undefined"
-            const isOffline =
-              isBrowser && typeof navigator !== "undefined" && !navigator.onLine
-            console.log(
-              "retry",
-              failureCount,
-              isBrowser ? "client" : "server",
-              status,
-              id,
-              "network error:",
-              isNetworkError,
-              "id in browser:",
-              isBrowser ? id : undefined,
-              "offline:",
-              isOffline,
-            )
-            /**
-             * React Query's default behavior is to retry all failed queries 3
-             * times. Many things (e.g., 403, 404) are not worth retrying. Let's
-             * just retry some explicit whitelist of status codes.
-             *
-             * Includes status undefined as we want to retry on network errors
-             */
-            if (status === undefined || !NO_RETRY_CODES.includes(status)) {
+
+            if (isNetworkError || !NO_RETRY_CODES.includes(status)) {
               return failureCount < MAX_RETRIES
             }
             return false
