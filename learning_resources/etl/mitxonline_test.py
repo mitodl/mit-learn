95f812b1889114cf33b747a8d10b2a7cc3ddb163
shankar ambady <ambady@mit.edu>
2025-05-07 15:16:56 -0400

    fixing test

--- learning_resources/etl/mitxonline_test.py
+++ learning_resources/etl/mitxonline_test.py
@@ -224,9 +224,6 @@ def test_mitxonline_transform_programs(
                             ]
                         )
                         > 0
-                        and any(
-                            run.get("enrollable") for run in course_data["courseruns"]
-                        )
                     ),
                     "certification": bool(
                         course_data.get("page", {}).get("page_url", None) is not None
@@ -354,7 +351,6 @@ def test_mitxonline_transform_courses(settings, mock_mitxonline_courses_data):
             "published": bool(
                 course_data.get("page", {}).get("page_url", None)
                 and course_data.get("page", {}).get("live", None)
-                and any(run.get("enrollable") for run in course_data["courseruns"])
             ),
             "professional": False,
             "certification": parse_certification(
