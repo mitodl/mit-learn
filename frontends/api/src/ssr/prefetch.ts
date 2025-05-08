4d24b1a8a2e692eb80b042de69945ef58453e76f
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2025-05-08 22:09:37 +0200

    Refactor for query client

--- frontends/api/src/ssr/prefetch.ts
+++ frontends/api/src/ssr/prefetch.ts
@@ -10,14 +10,48 @@ type MaybeHasStatus = {
   }
 }
 
+const getPrefetchQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        /**
+         * React Query's default retry logic is only active in the browser.
+         * Here we explicitly configure it to retry MAX_RETRIES times on
+         * the server, with an exclusion list of statuses that we expect not
+         * to succeed on retry.
+         *
+         * Includes status undefined as we want to retry on network errors
+         */
+        retry: (failureCount, error) => {
+          const status = (error as MaybeHasStatus)?.response?.status
+          const isNetworkError = status === undefined || status === 0
+
+          if (isNetworkError || !NO_RETRY_CODES.includes(status)) {
+            return failureCount < MAX_RETRIES
+          }
+          return false
+        },
+
+        /**
+         * By default, React Query gradually applies a backoff delay, though it is
+         * preferable that we do not significantly delay initial page renders (or
+         * indeed pages that are Statically Rendered during the build process) and
+         * instead allow the request to fail quickly so it can be subsequently
+         * fetched on the client.
+         */
+        retryDelay: 1000,
+      },
+    },
+  })
+}
+
 /* Utility to avoid repetition in server components
  * Optionally pass the queryClient returned from a previous prefetch
  * where queries are dependent on previous results
  */
 export const prefetch = async (
   queries: (Query | unknown)[],
-  queryClient?: QueryClient,
-) => {
+
   /**
    * The SSR QueryClient is transient - it is created only for prefetch
    * while API requests are made to server render the page and discarded
@@ -25,41 +59,8 @@ export const prefetch = async (
    *
    * Create a new query client if one is not provided.
    */
-  queryClient =
-    queryClient ||
-    new QueryClient({
-      defaultOptions: {
-        queries: {
-          /**
-           * React Query's default retry logic is only active in the browser.
-           * Here we explicitly configure it to retry MAX_RETRIES times on
-           * the server, with an exclusion list of statuses that we expect not
-           * to succeed on retry.
-           *
-           * Includes status undefined as we want to retry on network errors
-           */
-          retry: (failureCount, error) => {
-            const status = (error as MaybeHasStatus)?.response?.status
-            const isNetworkError = status === undefined || status === 0
-
-            if (isNetworkError || !NO_RETRY_CODES.includes(status)) {
-              return failureCount < MAX_RETRIES
-            }
-            return false
-          },
-
-          /**
-           * By default, React Query gradually applies a backoff delay, though it is
-           * preferable that we do not significantly delay initial page renders (or
-           * indeed pages that are Statically Rendered during the build process) and
-           * instead allow the request to fail quickly so it can be subsequently
-           * fetched on the client.
-           */
-          retryDelay: 1000,
-        },
-      },
-    })
-
+  queryClient = getPrefetchQueryClient(),
+) => {
   await Promise.all(
     queries
       .filter(Boolean)
