47e7b788559918d615500716bbd9aee9a122e63f
shankar ambady <ambady@mit.edu>
2025-06-20 11:07:16 -0400

    refactoring methods into canvas.py and making sure we get latest archive for course

--- learning_resources/etl/utils.py
+++ learning_resources/etl/utils.py
@@ -22,7 +22,6 @@ from tempfile import TemporaryDirectory
 import boto3
 import rapidjson
 import requests
-from defusedxml import ElementTree
 from django.conf import settings
 from django.utils.dateparse import parse_duration
 from django.utils.text import slugify
@@ -37,7 +36,6 @@ from learning_resources.constants import (
     DEPARTMENTS,
     VALID_TEXT_FILE_TYPES,
     LearningResourceDelivery,
-    LearningResourceType,
     LevelType,
     OfferedBy,
     RunStatus,
@@ -409,22 +407,6 @@ def text_from_sjson_content(content: str):
     return " ".join(data.get("text", []))
 
 
-def transform_canvas_content_files(
-    course_zipfile: Path, run: LearningResourceRun, *, overwrite
-) -> Generator[dict, None, None]:
-    """
-    Transform content files from a Canvas course zipfile
-    """
-    basedir = course_zipfile.name.split(".")[0]
-    with (
-        TemporaryDirectory(prefix=basedir) as olx_path,
-        zipfile.ZipFile(course_zipfile.absolute(), "r") as course_archive,
-    ):
-        for member in course_archive.infolist():
-            course_archive.extract(member, path=olx_path)
-        yield from _process_olx_path(olx_path, run, overwrite=overwrite)
-
-
 def _process_olx_path(olx_path: str, run: LearningResourceRun, *, overwrite):
     for document, metadata in documents_from_olx(olx_path):
         source_path = metadata.get("source_path")
@@ -486,74 +468,6 @@ def _process_olx_path(olx_path: str, run: LearningResourceRun, *, overwrite):
                 **content_dict,
             }
         )
-
-
-def sync_canvas_archive(bucket, key: str, overwrite):
-    """
-    Sync a Canvas course archive from S3
-    """
-    from learning_resources.etl.loaders import load_content_files
-
-    with TemporaryDirectory() as export_tempdir:
-        course_archive_path = Path(export_tempdir, key.split("/")[-1])
-        bucket.download_file(key, course_archive_path)
-        run = run_for_canvas_archive(course_archive_path, overwrite=overwrite)
-        checksum = calc_checksum(course_archive_path)
-        load_content_files(
-            run,
-            transform_canvas_content_files(
-                course_archive_path, run, overwrite=overwrite
-            ),
-        )
-
-        run.checksum = checksum
-        run.save()
-
-
-def run_for_canvas_archive(course_archive_path, overwrite):
-    """
-    Generate and return a LearningResourceRun for a Canvas course
-    """
-    checksum = calc_checksum(course_archive_path)
-    course_info = parse_canvas_settings(course_archive_path)
-    course_title = course_info.get("title")
-    readable_id = course_info.get("course_code")
-    # create placeholder learning resource
-    resource, _ = LearningResource.objects.get_or_create(
-        readable_id=readable_id,
-        defaults={
-            "title": course_title,
-            "published": False,
-            "test_mode": True,
-            "etl_source": ETLSource.canvas.name,
-            "resource_type": LearningResourceType.course.name,
-        },
-    )
-    if resource.runs.count() == 0:
-        LearningResourceRun.objects.create(
-            run_id=f"{readable_id}+canvas", learning_resource=resource, published=True
-        )
-    run = resource.runs.first()
-    if run.checksum == checksum and not overwrite:
-        log.info("Checksums match for %s, skipping load", readable_id)
-        return None
-    run.checksum = checksum
-    run.save()
-    return run
-
-
-def parse_canvas_settings(course_archive_path):
-    """
-    Get course attributes from a Canvas course archive
-    """
-    with zipfile.ZipFile(course_archive_path, "r") as course_archive:
-        xml_string = course_archive.read("course_settings/course_settings.xml")
-    tree = ElementTree.fromstring(xml_string)
-    attributes = {}
-    for node in tree.iter():
-        tag = node.tag.split("}")[1] if "}" in node.tag else node.tag
-        attributes[tag] = node.text
-    return attributes
 
 
 def transform_content_files(
