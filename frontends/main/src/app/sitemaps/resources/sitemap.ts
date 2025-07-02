25af276e307c721a1264b7d14113065bb2e7da3d
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-07-02 19:47:29 -0400

    skip build-time api calls

--- frontends/main/src/app/sitemaps/resources/sitemap.ts
+++ frontends/main/src/app/sitemaps/resources/sitemap.ts
@@ -2,13 +2,29 @@ import type { MetadataRoute } from "next"
 import { learningResourcesApi } from "api/clients"
 import invariant from "tiny-invariant"
 import { GenerateSitemapResult } from "../types"
+import { dangerouslyDetectProductionBuildPhase } from "../util"
 
 const BASE_URL = process.env.NEXT_PUBLIC_ORIGIN
 invariant(BASE_URL, "NEXT_PUBLIC_ORIGIN must be defined")
 
 const TRY_FOR_PAGE_SIZE = 10000
 
+/**
+ * By default in NextJS, sitemaps are statically generated at build time.
+ * We want to ensure up-to-date sitemaps.
+ *
+ * This forces the sitemaps to be rendered for each request.
+ * However, we we set s-maxage in the headers (next.config.js) to enable caching
+ * by a CDN.
+ */
+export const dynamic = "force-dynamic"
+
 export async function generateSitemaps(): Promise<GenerateSitemapResult[]> {
+  /**
+   * NextJS runs this at build time (despite force-dynamic below).
+   * Early exist here to avoid the useless build-time API calls.
+   */
+  if (dangerouslyDetectProductionBuildPhase()) return []
   const { count, results } = (
     await learningResourcesApi.learningResourcesSummaryList({
       limit: TRY_FOR_PAGE_SIZE,
@@ -42,13 +58,3 @@ export default async function sitemap({
     lastModified: resource.last_modified ?? undefined,
   }))
 }
-
-/**
- * By default in NextJS, sitemaps are statically generated at build time.
- * We want to ensure up-to-date sitemaps.
- *
- * This forces the sitemaps to be rendered for each request.
- * However, we we set s-maxage in the headers (next.config.js) to enable caching
- * by a CDN.
- */
-export const dynamic = "force-dynamic"
