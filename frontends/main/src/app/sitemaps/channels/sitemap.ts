6b5b535f20573d614f890b6bcbf36d0f91142a20
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-07-02 19:47:29 -0400

    skip build-time api calls

--- frontends/main/src/app/sitemaps/channels/sitemap.ts
+++ frontends/main/src/app/sitemaps/channels/sitemap.ts
@@ -2,13 +2,29 @@ import type { MetadataRoute } from "next"
 import { channelsApi } from "api/clients"
 import invariant from "tiny-invariant"
 import type { GenerateSitemapResult } from "../types"
+import { dangerouslyDetectProductionBuildPhase } from "../util"
 
 const BASE_URL = process.env.NEXT_PUBLIC_ORIGIN
 invariant(BASE_URL, "NEXT_PUBLIC_ORIGIN must be defined")
 
 const TRY_FOR_PAGE_SIZE = 100
 
+/**
+ * By default in NextJS, sitemaps are statically generated at build time.
+ * We want to ensure up-to-date sitemaps.
+ *
+ * This forces the sitemaps to be rendered for each request.
+ * However, we we set s-maxage in the headers (next.config.js) to enable caching
+ * by a CDN.
+ */
+export const dynamic = "force-dynamic"
+
 export async function generateSitemaps(): Promise<GenerateSitemapResult[]> {
+  /**
+   * NextJS runs this at build time (despite force-dynamic below).
+   * Early exist here to avoid the useless build-time API calls.
+   */
+  if (dangerouslyDetectProductionBuildPhase()) return []
   const { count, results } = (
     await channelsApi.channelsList({ limit: TRY_FOR_PAGE_SIZE })
   ).data
@@ -36,13 +52,3 @@ export default async function sitemap({
     url: channel.channel_url,
   }))
 }
-
-/**
- * By default in NextJS, sitemaps are statically generated at build time.
- * We want to ensure up-to-date sitemaps.
- *
- * This forces the sitemaps to be rendered for each request.
- * However, we we set s-maxage in the headers (next.config.js) to enable caching
- * by a CDN.
- */
-export const dynamic = "force-dynamic"
