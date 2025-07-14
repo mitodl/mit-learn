3dba5fccd6216d912e7e39418b4d418c90e49386
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-07-14 16:02:23 -0400

    refetch on focus or visibilitychange

--- frontends/main/src/app/getQueryClient.test.tsx
+++ frontends/main/src/app/getQueryClient.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { renderHook, waitFor } from "@testing-library/react"
+import { act, renderHook, waitFor } from "@testing-library/react"
 import { allowConsoleErrors } from "ol-test-utilities"
 import { QueryClientProvider, useQuery } from "@tanstack/react-query"
 import { makeQueryClient } from "./getQueryClient"
@@ -46,3 +46,78 @@ test.each([
     expect(queryFn).toHaveBeenCalledTimes(retries + 1)
   },
 )
+
+describe("refetching stale queries on visibilitychange and focus", () => {
+  let visibilityStateMock: jest.SpyInstance, documentHasFocus: jest.SpyInstance
+  beforeAll(() => {
+    visibilityStateMock = jest.spyOn(document, "visibilityState", "get")
+    documentHasFocus = jest.spyOn(document, "hasFocus")
+  })
+  afterAll(() => {
+    visibilityStateMock.mockRestore()
+    documentHasFocus.mockRestore()
+  })
+
+  const fireVisibilityChange = (state: "visible" | "hidden") => {
+    return act(() => {
+      visibilityStateMock.mockReturnValue(state)
+      window.dispatchEvent(new Event("visibilitychange"))
+      return new Promise((res) => setTimeout(res, 0))
+    })
+  }
+  const fireWindow = {
+    blur: () => {
+      return act(() => {
+        documentHasFocus.mockReturnValue(false)
+        window.dispatchEvent(new Event("blur"))
+        return new Promise((res) => setTimeout(res, 0))
+      })
+    },
+    focus: () => {
+      return act(() => {
+        documentHasFocus.mockReturnValue(true)
+        window.dispatchEvent(new Event("focus"))
+        return new Promise((res) => setTimeout(res, 0))
+      })
+    },
+  }
+
+  test("should refetch stale queries on window focus", async () => {
+    const wrapper = getWrapper()
+    const queryFn = jest.fn().mockResolvedValue({ message: "hi" })
+
+    renderHook(() => useQuery({ queryKey: ["test"], queryFn, staleTime: 0 }), {
+      wrapper,
+    })
+
+    await waitFor(() => {
+      expect(queryFn).toHaveBeenCalledTimes(1)
+    })
+
+    await fireWindow.blur()
+    expect(queryFn).toHaveBeenCalledTimes(1)
+    await fireWindow.focus()
+    expect(queryFn).toHaveBeenCalledTimes(2)
+    await fireWindow.focus()
+    expect(queryFn).toHaveBeenCalledTimes(2)
+  })
+  test("should refetch stale queries on visibilitychange", async () => {
+    const wrapper = getWrapper()
+    const queryFn = jest.fn().mockResolvedValue({ message: "hi" })
+
+    renderHook(() => useQuery({ queryKey: ["test"], queryFn, staleTime: 0 }), {
+      wrapper,
+    })
+
+    await waitFor(() => {
+      expect(queryFn).toHaveBeenCalledTimes(1)
+    })
+
+    await fireVisibilityChange("hidden")
+    expect(queryFn).toHaveBeenCalledTimes(1)
+    await fireVisibilityChange("visible")
+    expect(queryFn).toHaveBeenCalledTimes(2)
+    await fireVisibilityChange("visible")
+    expect(queryFn).toHaveBeenCalledTimes(2)
+  })
+})
