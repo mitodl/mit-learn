8377a152406a029f42047f24488d9d28bb35667f
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2025-07-21 14:59:19 +0200

    Disallow robot crawlers outside of Prod

--- frontends/main/src/app/robots.ts
+++ frontends/main/src/app/robots.ts
@@ -5,22 +5,29 @@ invariant(process.env.NEXT_PUBLIC_ORIGIN, "NEXT_PUBLIC_ORIGIN must be defined")
 const BASE_URL: string = process.env.NEXT_PUBLIC_ORIGIN
 
 export default function robots(): MetadataRoute.Robots {
-  const sitemap =
-    process.env.MITOL_NOINDEX === "false"
-      ? `${BASE_URL}/sitemaps/sitemap-index.xml`
-      : undefined
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: [
-        "/dashboard/",
-        "/learningpaths/",
-        "/onboarding/",
-        "/cart/",
-        "/program_letter/",
-      ],
-    },
-    sitemap,
+  const shouldIndex = process.env.MITOL_NOINDEX === "false"
+
+  if (shouldIndex) {
+    return {
+      rules: {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/dashboard/",
+          "/learningpaths/",
+          "/onboarding/",
+          "/cart/",
+          "/program_letter/",
+        ],
+      },
+      sitemap: `${BASE_URL}/sitemaps/sitemap-index.xml`,
+    }
+  } else {
+    return {
+      rules: {
+        userAgent: "*",
+        disallow: "/",
+      },
+    }
   }
 }
