208c9bb5a375fe4df15c087aa44d82c13501e3ec
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-07-22 08:08:01 -0400

    separate login and signup redirects

--- frontends/main/src/app-pages/HomePage/HomePage.test.tsx
+++ frontends/main/src/app-pages/HomePage/HomePage.test.tsx
@@ -283,9 +283,11 @@ describe("Home Page personalize section", () => {
     const link = within(personalize).getByRole("link")
     expect(link).toHaveAttribute(
       "href",
-      routes.login({
-        pathname: routes.DASHBOARD_HOME,
-        searchParams: null,
+      routes.auth({
+        loginNext: {
+          pathname: routes.DASHBOARD_HOME,
+          searchParams: null,
+        },
       }),
     )
   })
