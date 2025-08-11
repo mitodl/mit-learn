c41bb7dfd2f4f9937b7856a33617f80e01217a38
shankar ambady <ambady@mit.edu>
2025-08-11 13:07:31 -0400

    update docstring

--- vector_search/utils.py
+++ vector_search/utils.py
@@ -524,7 +524,8 @@ def embed_learning_resources(ids, resource_type, overwrite):
     else:
         serialized_resources = list(serialize_bulk_content_files(ids))
         # TODO: Pass actual Ids when we want scheduled content file summarization  # noqa: FIX002, TD002, TD003 E501
-        # Currently we only want to summarize content that already has a summary
+        # Currently we only want to summarize content that either already has a summary
+        # OR is in a course where atleast one other content file has a summary
         existing_summary_content_ids = [
             resource["id"]
             for resource in serialized_resources
