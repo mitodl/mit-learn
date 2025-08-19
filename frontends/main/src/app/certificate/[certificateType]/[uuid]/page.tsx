3ace17081ce22b06ec0752fdb2f4864b0e687242
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2025-08-19 19:30:48 +0200

    Certificate page UI (#2429)
    * Certificate page
    
    * Remove unused
    
    * Update to use fixed series name and product_name for main title
    
    * Updates for mobile design
    
    * Apply responsive design at tablet widths
    
    * Update paths to qualify course/program. Feed in data from updated certificate APIs
    
    * Clean up
    
    * Update mitxonline api client
    
    * fix upstream issue with course with course runs return type changing
    
    * More upstream fixes
    
    * Fix page props
    
    * Upgrade @mitodl/mitxonline-api-axios
    
    ---------
    
    Co-authored-by: Carey Gumaer <gumaerc@mit.edu>

---
+++ frontends/main/src/app/certificate/[certificateType]/[uuid]/page.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { notFound } from "next/navigation"
+import CertificatePage from "@/app-pages/CertificatePage/CertificatePage"
+
+enum CertificateType {
+  Course = "course",
+  Program = "program",
+}
+
+interface PageProps {
+  params: Promise<{
+    certificateType: CertificateType
+    uuid: string
+  }>
+}
+
+const Page: React.FC<PageProps> = async ({ params }) => {
+  const { certificateType } = await params
+
+  if (
+    ![CertificateType.Course, CertificateType.Program].includes(
+      certificateType as CertificateType,
+    )
+  ) {
+    notFound()
+  }
+
+  return <CertificatePage />
+}
+
+export default Page
