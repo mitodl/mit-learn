d950ad9dafcab2fdfec2f4a7204d6be79a6108f1
Carey Gumaer <gumaerc@mit.edu>
2025-09-09 15:19:55 -0400

    remove usage of onboarding_completed and use has_logged_in as the source of truth

--- authentication/views.py
+++ authentication/views.py
@@ -111,16 +111,13 @@ class CustomLoginView(View):
                     redirect_url = urljoin(
                         settings.APP_BASE_URL, f"/dashboard/organization/{org_slug}"
                     )
-            # Non-organization users: apply onboarding logic (but skip for attach URLs)
             elif (
-                not profile.completed_onboarding
+                not profile.has_logged_in
                 and request.GET.get("skip_onboarding", "0") == "0"
                 and not is_attach_url
             ):
                 params = urlencode({"next": redirect_url})
                 redirect_url = f"{settings.MITOL_NEW_USER_LOGIN_URL}?{params}"
-                profile.completed_onboarding = True
-                profile.save()
 
             if not profile.has_logged_in:
                 profile.has_logged_in = True
