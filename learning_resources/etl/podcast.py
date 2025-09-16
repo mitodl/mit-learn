eb99c74ee66adac640fb8d7ccced48c02fb63d9f
dansubak <dansubak@mit.edu>
2025-09-16 13:47:59 -0400

    Line too long

--- learning_resources/etl/podcast.py
+++ learning_resources/etl/podcast.py
@@ -20,7 +20,9 @@ CONFIG_FILE_REPO = "mitodl/open-podcast-data"
 CONFIG_FILE_FOLDER = "podcasts"
 TIMESTAMP_FORMAT = "%a, %d %b %Y  %H:%M:%S %z"
 BROWSER_UA_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/39.0.2171.95 Safari/537.36"
 }
 
 log = logging.getLogger()
