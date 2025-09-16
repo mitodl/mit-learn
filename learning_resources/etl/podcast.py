f3057f8a30f1bd126eb9bc8dde98d17e08f8f6ef
dansubak <dansubak@mit.edu>
2025-09-16 13:45:27 -0400

    User agent line too long

--- learning_resources/etl/podcast.py
+++ learning_resources/etl/podcast.py
@@ -23,7 +23,6 @@ BROWSER_UA_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
 }
 
-
 log = logging.getLogger()
 
 
