b3a479f889f5b1a12924772841ecb6d88ee8f000
shankar ambady <ambady@mit.edu>
2025-10-06 16:17:52 -0400

    fixing types

--- frontends/api/src/test-utils/factories/articles.ts
+++ frontends/api/src/test-utils/factories/articles.ts
@@ -3,13 +3,13 @@ import { makePaginatedFactory } from "ol-test-utilities"
 import type { Factory } from "ol-test-utilities"
 import type { RichTextArticle } from "../../generated/v1"
 
-const richTextArticle: Factory<RichTextArticle> = (overrides = {}) => ({
+const article: Factory<RichTextArticle> = (overrides = {}) => ({
   id: faker.number.int(),
   title: faker.lorem.sentence(),
   html: faker.lorem.paragraph(),
   ...overrides,
 })
 
-const richTextArticles = makePaginatedFactory(richTextArticle)
+const articles = makePaginatedFactory(article)
 
-export { richTextArticle, richTextArticles }
+export { article, articles }
