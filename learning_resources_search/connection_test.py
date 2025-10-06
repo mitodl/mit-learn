3304e31e86c67c4a8ad90d2a038d788b9536cc3b
shankar ambady <ambady@mit.edu>
2025-10-06 13:45:59 -0400

    fixing tests

--- learning_resources_search/connection_test.py
+++ learning_resources_search/connection_test.py
@@ -54,6 +54,8 @@ def test_get_active_aliases(mocker, index_types, indexes_exist, object_types):
                 "testindex_video_reindexing",
                 "testindex_video_playlist_default",
                 "testindex_video_playlist_reindexing",
+                "testindex_article_default",
+                "testindex_article_reindexing",
             ]
         elif index_types == IndexestoUpdate.current_index.value:
             assert active_aliases == [
@@ -63,6 +65,7 @@ def test_get_active_aliases(mocker, index_types, indexes_exist, object_types):
                 "testindex_podcast_default",
                 "testindex_podcast_episode_default",
                 "testindex_video_default",
+                "testindex_article_default",
                 "testindex_video_playlist_default",
             ]
         elif index_types == IndexestoUpdate.reindexing_index.value:
@@ -74,6 +77,7 @@ def test_get_active_aliases(mocker, index_types, indexes_exist, object_types):
                 "testindex_podcast_episode_reindexing",
                 "testindex_video_reindexing",
                 "testindex_video_playlist_reindexing",
+                "testindex_article_reindexing",
             ]
     else:
         assert active_aliases == []
