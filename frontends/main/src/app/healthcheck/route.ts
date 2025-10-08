4118408620857bf165845837740553255b748393
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-10-08 10:30:23 -0400

    Expose version of frontend (#2575)
    * expose version
    
    * add default version

--- frontends/main/src/app/healthcheck/route.ts
+++ frontends/main/src/app/healthcheck/route.ts
@@ -1,3 +1,11 @@
+const VERSION = process.env.NEXT_PUBLIC_VERSION || "unknown"
 export async function GET() {
-  return Response.json({ status: "ok" }, { status: 200 })
+  return Response.json(
+    {
+      status: "ok",
+      version: VERSION,
+      timestamp: new Date().toISOString(), // easily tell if response is cached
+    },
+    { status: 200 },
+  )
 }
