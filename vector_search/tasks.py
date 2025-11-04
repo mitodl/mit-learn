4500fd6d100412041392d289c2c9e940b2a284b0
shankar ambady <ambady@mit.edu>
2025-11-04 14:50:10 -0500

    adding safe getters

--- vector_search/tasks.py
+++ vector_search/tasks.py
@@ -384,10 +384,10 @@ def embeddings_healthcheck():
     remaining_resources = []
     content_file_point_ids = {}
     resource_point_ids = {}
-    total_resources = LearningResource.objects.filter(
+    all_resources = LearningResource.objects.filter(
         Q(published=True) | Q(test_mode=True)
-    ).count()
-    for lr in LearningResource.objects.filter(Q(published=True) | Q(test_mode=True)):
+    )
+    for lr in all_resources:
         run = (
             lr.best_run
             if lr.best_run
@@ -396,7 +396,7 @@ def embeddings_healthcheck():
         point_id = vector_point_id(lr.readable_id)
         resource_point_ids[point_id] = {"resource_id": lr.readable_id, "id": lr.id}
         if run:
-            for cf in run.content_files.all():
+            for cf in run.content_files.filter(published=True):
                 if cf and cf.content:
                     point_id = vector_point_id(
                         f"{lr.readable_id}.{run.run_id}.{cf.key}.0"
@@ -404,9 +404,7 @@ def embeddings_healthcheck():
                     content_file_point_ids[point_id] = {"key": cf.key, "id": cf.id}
 
     for batch in chunks(
-        LearningResource.objects.filter(published=True).values_list(
-            "readable_id", flat=True
-        ),
+        all_resources.values_list("readable_id", flat=True),
         chunk_size=200,
     ):
         remaining_resources.extend(
@@ -423,23 +421,17 @@ def embeddings_healthcheck():
         )
 
     remaining_content_file_ids = [
-        content_file_point_ids[p]["id"] for p in remaining_content_files
+        content_file_point_ids.get(p, {}).get("id") for p in remaining_content_files
     ]
 
-    remaining_resource_ids = [resource_point_ids[p]["id"] for p in remaining_resources]
+    remaining_resource_ids = [
+        resource_point_ids.get(p, {}).get("id") for p in remaining_resources
+    ]
 
     log.info("remaining content files - %d", len(remaining_content_files))
     log.info("remaining learning resources - %d", len(remaining_resources))
 
     if len(remaining_content_files) > 0:
-        log.info(
-            "percent complete (by contentfile) - %f",
-            (
-                (ContentFile.objects.all().count() - len(remaining_content_files))
-                / ContentFile.objects.all().count()
-            )
-            * 100,
-        )
         with sentry_sdk.new_scope() as scope:
             scope.set_context(
                 "missing_content_file_embeddings",
@@ -456,10 +448,6 @@ def embeddings_healthcheck():
                 "embeddings detected"
             )
     if len(remaining_resources) > 0:
-        log.info(
-            "percent complete (by resource) - %f",
-            ((total_resources - len(remaining_resources)) / total_resources) * 100,
-        )
         with sentry_sdk.new_scope() as scope:
             scope.set_context(
                 "missing_learning_resource_embeddings",
