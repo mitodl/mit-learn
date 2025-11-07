3ddc0c81546c609f82f0ff632c86c8fe75a5d439
shankar ambady <ambady@mit.edu>
2025-11-07 18:21:50 -0500

    adding display facet to opensearch

--- learning_resources_search/constants.py
+++ learning_resources_search/constants.py
@@ -194,6 +194,7 @@ LEARNING_RESOURCE_MAP = {
         "properties": {
             "code": {"type": "keyword"},
             "name": {"type": "keyword"},
+            "display_facet": {"type": "boolean"},
             "channel_url": {"type": "keyword"},
         },
     },
