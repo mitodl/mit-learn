65dd4179d4cc5e945db3d729732b13c7c68d6462
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2025-11-10 18:32:35 +0000

    Ignore stylelint errors in vendor sheets

--- frontends/main/src/app-pages/ArticlePage/NewArticlePage.tsx
+++ frontends/main/src/app-pages/ArticlePage/NewArticlePage.tsx
@@ -2,8 +2,9 @@
 
 import React from "react"
 import { theme, styled, HEADER_HEIGHT } from "ol-components"
-
 import { TiptapEditor } from "ol-components"
+import { Permission } from "api/hooks/user"
+import RestrictedRoute from "@/components/RestrictedRoute/RestrictedRoute"
 
 const PageContainer = styled.div({
   color: theme.custom.colors.darkGray2,
@@ -23,11 +24,13 @@ const StyledTiptapEditor = styled(TiptapEditor)({
 
 const NewArticlePage: React.FC = () => {
   return (
-    <PageContainer>
-      <EditorContainer>
-        <StyledTiptapEditor />
-      </EditorContainer>
-    </PageContainer>
+    <RestrictedRoute requires={Permission.ArticleEditor}>
+      <PageContainer>
+        <EditorContainer>
+          <StyledTiptapEditor />
+        </EditorContainer>
+      </PageContainer>
+    </RestrictedRoute>
   )
 }
 
