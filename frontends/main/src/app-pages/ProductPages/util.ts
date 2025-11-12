bc024159e746ba0a7a1b9e8e3d8b608bde22cbef
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-11-12 12:38:45 -0500

    display requirement cards

--- frontends/main/src/app-pages/ProductPages/util.ts
+++ frontends/main/src/app-pages/ProductPages/util.ts
@@ -10,11 +10,12 @@ enum HeadingIds {
   Prereqs = "prerequisites",
   Instructors = "instructors",
   WhoCanTake = "who-can-take",
+  Requirements = "requirements",
+  RequirementsRequired = "required-courses",
+  RequirementsElectives = "elective-courses",
 }
 
-const getElectiveSubtree = (
-  program: V2Program,
-): V2ProgramRequirement | undefined => {
+const getSubtree = (program: V2Program, type: "required" | "elective") => {
   const find = (
     nodes: V2ProgramRequirement[],
   ): V2ProgramRequirement | undefined =>
@@ -22,23 +23,11 @@ const getElectiveSubtree = (
       (child) =>
         child.data.node_type ===
           V2ProgramRequirementDataNodeTypeEnum.Operator &&
-        child.data.elective_flag,
-    )
-  return find(program.req_tree) ?? find(program.req_tree[0]?.children || [])
-}
-const getRequiredSubtree = (
-  program: V2Program,
-): V2ProgramRequirement | undefined => {
-  const find = (
-    nodes: V2ProgramRequirement[],
-  ): V2ProgramRequirement | undefined =>
-    nodes?.find(
-      (child) =>
-        child.data.node_type ===
-          V2ProgramRequirementDataNodeTypeEnum.Operator &&
-        !child.data.elective_flag,
+        (type === "elective"
+          ? child.data.elective_flag
+          : !child.data.elective_flag),
     )
   return find(program.req_tree) ?? find(program.req_tree[0]?.children || [])
 }
 
-export { HeadingIds, getElectiveSubtree, getRequiredSubtree }
+export { HeadingIds, getSubtree }
