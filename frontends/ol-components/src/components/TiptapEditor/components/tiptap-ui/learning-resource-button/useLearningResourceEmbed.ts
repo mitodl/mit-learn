618e91f208505daba7c5fa59cfbe734c14358d46
Ahtesham Quraish <ahtesham.quraish@A006-01455.local>
2025-11-20 13:25:46 +0500

    added modal for user input

--- frontends/ol-components/src/components/TiptapEditor/components/tiptap-ui/learning-resource-button/useLearningResourceEmbed.ts
+++ frontends/ol-components/src/components/TiptapEditor/components/tiptap-ui/learning-resource-button/useLearningResourceEmbed.ts
@@ -1,6 +1,8 @@
 import { useCallback } from "react"
 import type { Editor } from "@tiptap/react"
 import { useTiptapEditor } from "../../../hooks/use-tiptap-editor"
+import NiceModal from "@ebay/nice-modal-react"
+import CourseUrlInputDialog from "./ResourceUrlInputDialog"
 import { Icon } from "./Icon" // you create this SVG
 
 export const LEARNING_RESOURCE_SHORTCUT_KEY = "Mod+Shift+R"
@@ -12,8 +14,9 @@ export function useLearningResourceEmbed(editor?: Editor | null) {
   const canInsert = resolved?.isEditable ?? false
   const label = "Insert Learning Resource"
 
-  const handleEmbed = useCallback(() => {
-    const url = prompt("Paste a Learn.MIT resource URL")
+  const handleEmbed = useCallback(async () => {
+    const url: string = await NiceModal.show(CourseUrlInputDialog)
+    console.log("URL received from modal:", url)
     if (!url) return
 
     // Extract `resource=XYZ`
