800c66e24de83d5697fe4676a1ad23c981fc7469
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2025-12-05 19:18:53 +0100

    Add back min height

--- frontends/main/src/app-pages/Articles/ArticleNewPage.tsx
+++ frontends/main/src/app-pages/Articles/ArticleNewPage.tsx
@@ -4,12 +4,21 @@ import React from "react"
 import { useRouter } from "next-nprogress-bar"
 import { Permission } from "api/hooks/user"
 import RestrictedRoute from "@/components/RestrictedRoute/RestrictedRoute"
-import { ArticleEditor, styled } from "ol-components"
+import {
+  ArticleEditor,
+  HEADER_HEIGHT,
+  HEADER_HEIGHT_MD,
+  styled,
+} from "ol-components"
 import { articlesView } from "@/common/urls"
 
 const PageContainer = styled.div(({ theme }) => ({
   color: theme.custom.colors.darkGray2,
   display: "flex",
+  minHeight: `calc(100vh - ${HEADER_HEIGHT}px - 132px)`,
+  [theme.breakpoints.down("md")]: {
+    minHeight: `calc(100vh - ${HEADER_HEIGHT_MD}px - 132px)`,
+  },
 }))
 
 const ArticleNewPage: React.FC = () => {
