f5b006dd43e171334fe550f3cfde59abb2bd0654
Ahtesham Quraish <ahtesham.quraish@192.168.1.5>
2026-01-07 19:37:44 +0500

    feat: add celery task for creating the news from article

--- news_events/tasks.py
+++ news_events/tasks.py
@@ -45,3 +45,29 @@ def get_mitpe_events():
     """Run the MIT Professional Education events ETL pipeline"""
     pipelines.mitpe_events_etl()
     clear_views_cache()
+
+
+@app.task
+def get_articles_news():
+    """Run the Articles News ETL pipeline"""
+    pipelines.articles_news_etl()
+    clear_views_cache()
+
+
+@app.task
+def sync_article_to_news(article_id: int):
+    """
+    Sync a single article to the news feed.
+
+    Args:
+        article_id (int): The ID of the article to sync
+    """
+    from articles.models import Article
+    from news_events.etl.articles_news import sync_single_article_to_news
+
+    try:
+        article = Article.objects.get(id=article_id, is_published=True)
+        sync_single_article_to_news(article)
+        clear_views_cache()
+    except Article.DoesNotExist:
+        pass  # Article was deleted or unpublished, skip sync
