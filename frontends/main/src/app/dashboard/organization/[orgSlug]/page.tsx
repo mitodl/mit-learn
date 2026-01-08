e3d8e54189929e94e6444f11b325ebad17754ce6
Carey Gumaer <gumaerc@mit.edu>
2026-01-08 17:41:26 -0500

    add a redirect at the org root that either goes to the first contract for that org or back to the dashboard home if no contracts are found

---
+++ frontends/main/src/app/dashboard/organization/[orgSlug]/page.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import React, { use, useEffect } from "react"
+import ContractContent from "@/app-pages/DashboardPage/ContractContent"
+import invariant from "tiny-invariant"
+import { useQuery } from "@tanstack/react-query"
+import { mitxUserQueries } from "api/mitxonline-hooks/user"
+import { matchOrganizationBySlug } from "@/common/utils"
+import { useRouter } from "next-nprogress-bar"
+
+const Page: React.FC<{
+  params: Promise<{ orgSlug: string }>
+}> = ({ params }) => {
+  const router = useRouter()
+  const { isLoading: isLoadingMitxOnlineUser, data: mitxOnlineUser } = useQuery(
+    mitxUserQueries.me(),
+  )
+
+  const resolved = use(params)
+  invariant(resolved.orgSlug, "orgSlug is required")
+  const orgSlug = resolved.orgSlug
+
+  const b2bOrganization = mitxOnlineUser?.b2b_organizations.find(
+    matchOrganizationBySlug(orgSlug),
+  )
+  const firstContractSlug = b2bOrganization?.contracts[0]?.slug
+
+  useEffect(() => {
+    if (!isLoadingMitxOnlineUser && !firstContractSlug) {
+      router.replace("/dashboard")
+    }
+  }, [isLoadingMitxOnlineUser, firstContractSlug, router])
+
+  if (firstContractSlug) {
+    return (
+      <ContractContent orgSlug={orgSlug} contractSlug={firstContractSlug} />
+    )
+  }
+
+  return null
+}
+
+export default Page
