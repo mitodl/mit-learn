b58956e2ca0f09023ae3ed11761efe94c734b630
Chris Chudzicki <christopher.chudzicki@gmail.com>
2026-01-13 10:04:29 -0500

    update mitxonline-api-axios client (#2852)

--- frontends/main/src/app-pages/ProductPages/util.ts
+++ frontends/main/src/app-pages/ProductPages/util.ts
@@ -1,5 +1,5 @@
 import type { V2Program } from "@mitodl/mitxonline-api-axios/v2"
-import { V2ProgramRequirementDataNodeTypeEnum } from "@mitodl/mitxonline-api-axios/v2"
+import { NodeTypeEnum } from "@mitodl/mitxonline-api-axios/v2"
 
 enum HeadingIds {
   About = "about",
@@ -20,34 +20,21 @@ type RequirementData = {
 }
 
 const parseReqTree = (reqTree: V2Program["req_tree"]): RequirementData[] => {
-  if (
-    !reqTree.every(
-      (node) =>
-        node.data.node_type === V2ProgramRequirementDataNodeTypeEnum.Operator,
-    )
-  ) {
+  if (!reqTree.every((node) => node.data.node_type === NodeTypeEnum.Operator)) {
     console.error(
       "UI Display expects program requirements root to only have operator children",
     )
   }
 
   return reqTree
-    .filter(
-      (node) =>
-        node.data.node_type === V2ProgramRequirementDataNodeTypeEnum.Operator,
-    )
+    .filter((node) => node.data.node_type === NodeTypeEnum.Operator)
     .map((node) => {
       const elective = node.data.elective_flag ?? false
       const title =
         node.data.title || (elective ? "Elective Courses" : "Core Courses")
 
       const children = node.children ?? []
-      if (
-        !children.every(
-          (c) =>
-            c.data.node_type === V2ProgramRequirementDataNodeTypeEnum.Course,
-        )
-      ) {
+      if (!children.every((c) => c.data.node_type === NodeTypeEnum.Course)) {
         console.error(
           "UI Display expects program requirements operator nodes to only have course children",
         )
