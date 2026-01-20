253c89fb0871af272e136f42027777288c00714f
Ahtesham Quraish <ahtesham.quraish@192.168.10.71>
2026-01-20 15:55:19 +0500

    add dialog and button for learning resource input

--- frontends/main/src/page-components/TiptapEditor/TiptapEditor.tsx
+++ frontends/main/src/page-components/TiptapEditor/TiptapEditor.tsx
@@ -53,6 +53,7 @@ import { BannerViewer } from "./extensions/node/Banner/BannerNode"
 import { ArticleByLineInfoBarViewer } from "./extensions/node/ArticleByLineInfoBar/ArticleByLineInfoBarViewer"
 import { ImageWithCaptionViewer } from "./extensions/node/Image/ImageWithCaption"
 import { DividerViewer } from "./extensions/node/Divider/DividerNode"
+import { LearningResourceButton } from "./extensions/ui/LearningResource/LearningResourceButton"
 import { LearningResourceCardViewer } from "./extensions/node/LearningResource/LearningResourceNode"
 
 const Container = styled.div<{
@@ -195,6 +196,9 @@ export function InsertDropdownMenu({ editor }: TiptapEditorToolbarProps) {
           <MediaEmbedButton editor={editor} text="Video" />
         </DropdownMenuItem>
 
+        <DropdownMenuItem asChild>
+          <LearningResourceButton editor={editor} text="Course Card" />
+        </DropdownMenuItem>
         <DropdownMenuItem asChild>
           <DividerButton editor={editor} text="Divider" />
         </DropdownMenuItem>
