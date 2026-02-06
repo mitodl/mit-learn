8d0271f1e18cbb2ff0b7bdd046f0824a15d5c29c
Chris Chudzicki <christopher.chudzicki@gmail.com>
2026-02-06 15:03:38 -0500

    whoopsie-daisy

--- frontends/main/src/app-pages/DashboardPage/CoursewareDisplay/DashboardCard.tsx
+++ frontends/main/src/app-pages/DashboardPage/CoursewareDisplay/DashboardCard.tsx
@@ -449,7 +449,6 @@ const UpgradeBanner: React.FC<
       // Reset mutation state to allow retry after error
       addToBasket.reset()
       clearBasket.reset()
-      throw new Error("Woof")
 
       await clearBasket.mutateAsync()
       await addToBasket.mutateAsync(productId)
