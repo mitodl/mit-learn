442f8d129ae0a54653dcf70d8d22968e0161754f
dansubak <dansubak@mit.edu>
2026-02-06 14:23:56 -0500

    Working example of program page test against local using test_datagen script

--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -1,5 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies,import/no-duplicates,import/no-restricted-paths */
 import { test, expect } from "@playwright/test"
+const LOCAL_DEFAULT = "http://nginx:8063"
+const RC_DEFAULT = "http://rc.learn.mit.edu"
+const KNOWN_BASE_URLS = [LOCAL_DEFAULT, RC_DEFAULT]
 
 test.describe("Smoke Test - Homepage", () => {
   test("should load the homepage successfully", async ({ page }) => {
@@ -9,32 +12,58 @@ test.describe("Smoke Test - Homepage", () => {
   })
 })
 
+const programPageB2CTestInfo = {
+  [RC_DEFAULT]: {
+    url: "/programs/program-v1:MITx+CTL.SCM",
+    title: "Supply Chain Management",
+    description:
+      "Gain expertise in the growing field of Supply Chain Management through an innovative online program consisting of five courses and a final capstone exam.",
+    courseCount: 6,
+  },
+  [LOCAL_DEFAULT]: {
+    url: "/programs/program-v1:PLACEHOLDER+PROGRAM",
+    title: "PLACEHOLDER - Data, Economics and Development Policy",
+    description:
+      "PLACEHOLDER - In this engineering program, we will explore the processing and structure of cellular solids as they are created from polymers, metals, ceramics, glasses and composites.",
+    courseCount: 2,
+  },
+}
+
+async function getProgramPageB2CTestInfo(baseUrl) {
+  let urlToLookup = baseUrl
+  if (!KNOWN_BASE_URLS.includes(baseUrl)) {
+    urlToLookup = LOCAL_DEFAULT
+  }
+  return programPageB2CTestInfo[urlToLookup]
+}
+
 test.describe("Smoke Test - Program Page B2C", () => {
-  test("should load the page successfully", async ({ page }) => {
-    // TODO: This will need to be parameterized based on expected for its environment
-    await page.goto("/programs/program-v1:MITx+CTL.SCM")
+  test("should load the page successfully", async ({ page }, testInfo) => {
+    const { url, title, description, courseCount } =
+      await getProgramPageB2CTestInfo(testInfo.config.projects[0].use.baseURL)
+    await page.goto(url)
 
     // This will only pass on RC as written.
     await expect(page.locator("main")).toBeVisible()
 
     // Find the program title
-    await expect(page.locator("h1")).toHaveText("Supply Chain Management")
+    await expect(page.locator("h1")).toHaveText(title)
     const about = page.locator("#about")
     await expect(about).toBeVisible()
     // Find the program description
     await expect(about.locator("//following-sibling::*[1]")).toContainText(
-      "Gain expertise in the growing field of Supply Chain Management through an innovative online program consisting of five courses and a final capstone exam.",
+      description,
     )
     // Assert that we have multiple courses
     const coursesHeader = page.locator("#required-courses")
     await expect(coursesHeader).toBeVisible()
     // This should be an unordered list containing multiple courses
     const requiredCourses = coursesHeader.locator("//following-sibling::*[1]")
-    await expect(requiredCourses.locator("li")).toHaveCount(6)
+    await expect(requiredCourses.locator("li")).toHaveCount(courseCount)
     // Assert that it is enrollable
     await expect(
       page.getByRole("button", { name: "Enroll for Free" }),
-    ).toBeVisible()
+    ).toBeVisible({ timeout: 30_000 })
     // Certificate Track label only exists if there's a price afaict
     // Should we have a stronger assertion? The value comes from the CMS, so it can change underneath us.
     await expect(page.getByText("Certificate Track")).toBeVisible()
