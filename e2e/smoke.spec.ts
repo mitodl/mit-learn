7ff946017f85d470fef99059300d4d10087bbfbb
dansubak <dansubak@mit.edu>
2026-02-06 15:03:14 -0500

    Add course test spec

--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies,import/no-duplicates,import/no-restricted-paths */
 import { test, expect } from "@playwright/test"
 const LOCAL_DEFAULT = "http://nginx:8063"
-const RC_DEFAULT = "http://rc.learn.mit.edu"
+const RC_DEFAULT = "https://rc.learn.mit.edu"
 const KNOWN_BASE_URLS = [LOCAL_DEFAULT, RC_DEFAULT]
 
 test.describe("Smoke Test - Homepage", () => {
@@ -12,6 +12,11 @@ test.describe("Smoke Test - Homepage", () => {
   })
 })
 
+/*
+For now, we're going to parameterize the test values we're going to assert
+Longer term, we probably want to use the same test data generation process in RC, meaning we won't need this layer of indirection
+If we keep this, we probably want to load this from a file or come up w/ a less annoying fixture setup
+*/
 const programPageB2CTestInfo = {
   [RC_DEFAULT]: {
     url: "/programs/program-v1:MITx+CTL.SCM",
@@ -29,12 +34,35 @@ const programPageB2CTestInfo = {
   },
 }
 
+const coursePageB2CTestInfo = {
+  [RC_DEFAULT]: {
+    url: "/courses/course-v1:MITxT+14.740x",
+    title: "Foundations of Development Policy",
+    description:
+      "This course is part of the MITx MicroMasters program in Data, Economics, and Development Policy (DEDP).",
+  },
+  [LOCAL_DEFAULT]: {
+    url: "/courses/course-v1:PLACEHOLDER+COURSE+IN+PROGRAM+ELECTIVE",
+    title: "PLACEHOLDER - Demonstration Course in Program (Elective)",
+    description:
+      "PLACEHOLDER - In this engineering course, we will explore the processing and structure of cellular solids as they are created from polymers, metals, ceramics, glasses and composites.",
+  },
+}
+
 async function getProgramPageB2CTestInfo(baseUrl) {
   let urlToLookup = baseUrl
   if (!KNOWN_BASE_URLS.includes(baseUrl)) {
     urlToLookup = LOCAL_DEFAULT
   }
   return programPageB2CTestInfo[urlToLookup]
+}
+
+async function getCoursePageB2CTestInfo(baseUrl) {
+  let urlToLookup = baseUrl
+  if (!KNOWN_BASE_URLS.includes(baseUrl)) {
+    urlToLookup = LOCAL_DEFAULT
+  }
+  return coursePageB2CTestInfo[urlToLookup]
 }
 
 test.describe("Smoke Test - Program Page B2C", () => {
@@ -67,5 +95,30 @@ test.describe("Smoke Test - Program Page B2C", () => {
     // Certificate Track label only exists if there's a price afaict
     // Should we have a stronger assertion? The value comes from the CMS, so it can change underneath us.
     await expect(page.getByText("Certificate Track")).toBeVisible()
+  })
+})
+
+test.describe("Smoke Test - Course Page B2C", () => {
+  test("should load the page successfully", async ({ page }, testInfo) => {
+    const { url, title, description } = await getCoursePageB2CTestInfo(
+      testInfo.config.projects[0].use.baseURL,
+    )
+    await page.goto(url)
+
+    await expect(page.locator("main")).toBeVisible()
+
+    // Find the program title
+    await expect(page.locator("h1")).toHaveText(title)
+    const about = page.locator("#about")
+    await expect(about).toBeVisible()
+    // Find the program description
+    await expect(about.locator("//following-sibling::*[1]")).toContainText(
+      description,
+    )
+
+    // Assert that it is enrollable
+    await expect(
+      page.getByRole("button", { name: "Enroll for Free" }),
+    ).toBeVisible({ timeout: 30_000 })
   })
 })
