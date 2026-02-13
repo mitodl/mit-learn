677f74d9b9c7e3fc24e8daa49c0c36d709126434
Matt Bertrand <mattbert@mit.edu>
2026-02-13 14:45:55 -0500

    faster startup

--- scripts/run-django-dev.sh
+++ scripts/run-django-dev.sh
@@ -7,6 +7,9 @@ python3 manage.py collectstatic --noinput --clear
 # run initial django migrations
 python3 manage.py migrate --noinput
 
+uwsgi uwsgi.ini --honour-stdin &
+UWSGI_PID=$!
+
 # populate cache table
 python3 manage.py createcachetable
 
@@ -23,4 +26,4 @@ python manage.py create_qdrant_collections
 # consolidate user subscriptions and remove duplicate percolate instances
 python manage.py prune_subscription_queries 2>&1
 
-uwsgi uwsgi.ini --honour-stdin
+wait $UWSGI_PID
