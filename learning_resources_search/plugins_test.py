60f299a520ef5db59c60db8ac1b5e90d74f00479
Matt Bertrand <mattbert@mit.edu>
2026-03-05 10:27:36 -0500

    Avoid unnecessary task calls for contentfiles if none exist

--- learning_resources_search/plugins_test.py
+++ learning_resources_search/plugins_test.py
@@ -5,6 +5,7 @@ from types import SimpleNamespace
 import pytest
 
 from learning_resources.factories import (
+    ContentFileFactory,
     LearningResourceFactory,
     LearningResourceRunFactory,
 )
@@ -71,11 +72,15 @@ def test_search_index_plugin_resource_upserted(
 
 @pytest.mark.django_db
 @pytest.mark.parametrize("resource_type", [COURSE_TYPE, PROGRAM_TYPE])
+@pytest.mark.parametrize("has_content_files", [True, False])
 def test_search_index_plugin_resource_unpublished(
-    mocker, mock_search_index_helpers, resource_type
+    mocker, mock_search_index_helpers, resource_type, has_content_files
 ):
     """The plugin function should remove a resource from the search index"""
     resource = LearningResourceFactory.create(resource_type=resource_type)
+    if resource_type == COURSE_TYPE and has_content_files:
+        for run in resource.runs.all():
+            ContentFileFactory.create(run=run)
     unpublish_run_mock = mocker.patch(
         "learning_resources_search.plugins.tasks.deindex_run_content_files.si"
     )
@@ -83,7 +88,7 @@ def test_search_index_plugin_resource_unpublished(
     mock_search_index_helpers.mock_remove_learning_resource_immutable_signature.assert_called_once_with(
         resource.id, resource.resource_type
     )
-    if resource_type == COURSE_TYPE:
+    if resource_type == COURSE_TYPE and has_content_files:
         assert unpublish_run_mock.call_count == resource.runs.count()
         for run in resource.runs.all():
             unpublish_run_mock.assert_any_call(run.id, unpublished_only=False)
@@ -107,26 +112,42 @@ def test_search_index_plugin_resource_before_delete(
 
 
 @pytest.mark.django_db
-def test_search_index_plugin_resource_run_unpublished(mock_search_index_helpers):
+@pytest.mark.parametrize("has_content_files", [True, False])
+def test_search_index_plugin_resource_run_unpublished(
+    mock_search_index_helpers, has_content_files
+):
     """The plugin function should remove a run's contenfiles from the search index"""
     run = LearningResourceRunFactory.create()
+    if has_content_files:
+        ContentFileFactory.create(run=run)
     SearchIndexPlugin().resource_run_unpublished(run)
-    mock_search_index_helpers.mock_remove_contentfiles_immutable_signature.assert_called_once_with(
-        run.id,
-        unpublished_only=False,
-    )
+    if has_content_files:
+        mock_search_index_helpers.mock_remove_contentfiles_immutable_signature.assert_called_once_with(
+            run.id,
+            unpublished_only=False,
+        )
+    else:
+        mock_search_index_helpers.mock_remove_contentfiles_immutable_signature.assert_not_called()
 
 
 @pytest.mark.django_db
-def test_search_index_plugin_resource_run_delete(mock_search_index_helpers):
+@pytest.mark.parametrize("has_content_files", [True, False])
+def test_search_index_plugin_resource_run_delete(
+    mock_search_index_helpers, has_content_files
+):
     """The plugin function should remove contenfiles from the index and delete the run"""
     run = LearningResourceRunFactory.create()
+    if has_content_files:
+        ContentFileFactory.create(run=run)
     run_id = run.id
     SearchIndexPlugin().resource_run_delete(run)
-    mock_search_index_helpers.mock_remove_contentfiles_immutable_signature.assert_called_once_with(
-        run_id,
-        unpublished_only=False,
-    )
+    if has_content_files:
+        mock_search_index_helpers.mock_remove_contentfiles_immutable_signature.assert_called_once_with(
+            run_id,
+            unpublished_only=False,
+        )
+    else:
+        mock_search_index_helpers.mock_remove_contentfiles_immutable_signature.assert_not_called()
     assert LearningResourceRun.objects.filter(id=run_id).exists() is False
 
 
