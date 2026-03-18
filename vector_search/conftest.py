154d85ae27f83241fddcdd102296fb9b54aa3969
shankar ambady <ambady@mit.edu>
2026-03-18 16:03:16 -0400

    pinning tokenizer to None for tests

--- vector_search/conftest.py
+++ vector_search/conftest.py
@@ -31,6 +31,7 @@ def _use_test_qdrant_settings(settings, mocker):
     settings.QDRANT_HOST = "https://test"
     settings.QDRANT_BASE_COLLECTION_NAME = "test"
     settings.LITELLM_API_BASE = "https://test/api/"
+    settings.LITELLM_TOKEN_ENCODING_NAME = None
     settings.CONTENT_FILE_EMBEDDING_CHUNK_OVERLAP = 0
     settings.CONTENT_FILE_EMBEDDING_SEMANTIC_CHUNKING_ENABLED = False
     settings.QDRANT_SPARSE_MODEL = "sklearn/hashing_vectorizer_sparse_model"
