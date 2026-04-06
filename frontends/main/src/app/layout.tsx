1607f0955143a77a5b50055e755beaae386eae41
CP <collinp@mit.edu>
2026-04-06 15:10:52 -0400

    code review comments

--- frontends/main/src/app/layout.tsx
+++ frontends/main/src/app/layout.tsx
@@ -59,17 +59,17 @@ export default function RootLayout({
             <Footer />
           </PageWrapper>
         </Providers>
+        {process.env.NEXT_PUBLIC_APPZI_URL ? (
+          <Script async src={process.env.NEXT_PUBLIC_APPZI_URL} />
+        ) : null}
+        {process.env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID ? (
+          <Script
+            id="hs-script-loader"
+            src={`https://js.hs-scripts.com/${process.env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID}.js`}
+            strategy="afterInteractive"
+          />
+        ) : null}
       </body>
-      {process.env.NEXT_PUBLIC_APPZI_URL ? (
-        <Script async src={process.env.NEXT_PUBLIC_APPZI_URL} />
-      ) : null}
-      {process.env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID ? (
-        <Script
-          id="hs-script-loader"
-          src={`https://js.hs-scripts.com/${process.env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID}.js`}
-          strategy="afterInteractive"
-        />
-      ) : null}
     </html>
   )
 }
