7313493b41231fd3f3997cede29e3d2aa5274350
shankar ambady <ambady@mit.edu>
2026-04-09 11:11:31 -0400

    add gensim encoder

---
+++ vector_search/encoders/gensim.py
@@ -0,0 +1,22 @@
+import gensim.downloader as api
+import numpy as np
+
+from vector_search.encoders.base import BaseEncoder
+
+
+class GensimEncoder(BaseEncoder):
+    """
+    Dummy Encoder
+    """
+
+    def __init__(self, model_name="glove/glove-wiki-gigaword-50"):
+        self.model_name = model_name
+        self.model = api.load(self.model_short_name())
+
+    def embed_documents(self, documents):
+        return [self.embed(doc) for doc in documents]
+
+    def embed(self, text):
+        words = text.lower().split()
+        vectors = [self.model[w] for w in words if w in self.model]
+        return np.mean(vectors, axis=0)
