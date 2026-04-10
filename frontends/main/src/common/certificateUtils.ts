3dc03c8b1d3ba5124d8a4753e0a393ba7aaa05a9
dansubak <dansubak@mit.edu>
2026-04-10 15:44:27 -0400

    WIP add to profile button

--- frontends/main/src/common/certificateUtils.ts
+++ frontends/main/src/common/certificateUtils.ts
@@ -6,3 +6,24 @@ export const getCertificateInfo = (): { displayType: string } => {
     displayType: "Certificate",
   }
 }
+
+export const getVerifiableCredentialLinkedInURL = (
+  verifiableCredentialJson: object,
+): string => {
+  // TODO: Need to decide if we want to use this as the cert ID.
+  const certId = verifiableCredentialJson["id"].substring(9)
+  const credentialName =
+    verifiableCredentialJson["credentialSubject"]["achievement"]["name"]
+  const orgName = verifiableCredentialJson["issuer"]["name"]
+  const issueYear = new Date(
+    verifiableCredentialJson["validFrom"],
+  ).getFullYear()
+  const issueMonth =
+    new Date(verifiableCredentialJson["validFrom"]).getMonth() + 1
+  // TODO: Need to figure out which URL we want to link.
+  // Also need to parameterize on whether or not its a course or a program cert if we go w/ the VC download API
+  const certUrl = `https://mitxonline.mit.edu/api/v2/verifiable_course_credential/${certId}/download`
+  return encodeURI(
+    `https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&name=${credentialName}&organizationName=${orgName}&issueYear=${issueYear}&issueMonth=${issueMonth}&certId=${certId}&certUrl=${certUrl}`,
+  )
+}
