af7216cb39a84f2bcd345276ca0d4cdb5270dbd9
Nathan Levesque <nlevesq@mit.edu>
2026-04-28 16:30:27 -0400

    Latest

--- load_testing/learn.smoke.ts
+++ load_testing/learn.smoke.ts
@@ -1,13 +1,19 @@
+import { IGNORE_HTTPS_ERRORS } from "./config.ts"
+
 export { testBackend } from "./backend/test.ts"
 export { testFrontend } from "./frontend/test.ts"
+
+const MAX_VUS = 4
+const BROWSER_VU_SHARE = 0.5
+const BACKEND_VU_SHARE = 1 - BROWSER_VU_SHARE
 
 export const options = {
   scenarios: {
     browser: {
       exec: "testFrontend",
       executor: "constant-vus",
-      vus: 10,
-      duration: "30s",
+      vus: Math.floor(MAX_VUS * BROWSER_VU_SHARE),
+      duration: "1m",
       options: {
         browser: {
           type: "chromium",
@@ -17,12 +23,13 @@ export const options = {
     backend: {
       exec: "testBackend",
       executor: "constant-vus",
-      vus: 10,
-      duration: "30s",
+      vus: Math.floor(MAX_VUS * BACKEND_VU_SHARE),
+      duration: "1m",
     },
   },
   thresholds: {
     // the rate of successful checks should be higher than 90%
     checks: ["rate>0.9"],
   },
+  insecureSkipTLSVerify: IGNORE_HTTPS_ERRORS,
 }
