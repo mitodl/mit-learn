ba604160596f4b765a79ae653a875b291cecc038
Nathan Levesque <nlevesq@mit.edu>
2026-05-20 12:56:37 -0400

    Last tests

--- profiles/api_test.py
+++ profiles/api_test.py
@@ -1,11 +1,13 @@
 """Profile API tests"""
 
 import pytest
+from mitol.keycloak.data_models import UserAttributes
 
 from main.factories import UserFactory
 from profiles import api
 from profiles.api import (
     get_site_type_from_url,
+    sync_to_keycloak,
 )
 from profiles.models import (
     FACEBOOK_DOMAIN,
@@ -22,7 +24,7 @@ pytestmark = pytest.mark.django_db
     "profile_data", [{"image": "http://localhost:image.jpg"}, {}, None]
 )
 @pytest.mark.parametrize("no_profile", [True, False])
-def test_ensure_profile(mocker, profile_data, no_profile):
+def test_ensure_profile(profile_data, no_profile):
     """Test that it creates a profile from the data"""
     user = UserFactory.create(email="testuser@example.com", no_profile=no_profile)
     profile = api.ensure_profile(user, profile_data=profile_data)
@@ -50,3 +52,20 @@ def test_ensure_profile(mocker, profile_data, no_profile):
 def test_get_site_type_from_url(url, exp_site_type):
     """Test that get_site_type_from_url returns the expected site type for a given URL value"""
     assert get_site_type_from_url(url) == exp_site_type
+
+
+def test_sync_to_keycloak(mocker):
+    """Test that syncing to keycloak correctly maps attributes"""
+    user = UserFactory.create()
+    mock_api = mocker.patch("profiles.api.keycloak_api")
+
+    sync_to_keycloak(user.profile, ["location"])
+    mock_api.update_user.assert_not_called()
+
+    sync_to_keycloak(user.profile, ["name"])
+    mock_api.update_user.assert_called_once_with(
+        user.global_id,
+        attributes=UserAttributes(
+            full_name=user.profile.name,
+        ),
+    )
