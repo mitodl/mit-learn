dc849f0299cd184480e371d100edf31ecc08fced
Doof <mitx-devops@mit.edu>
2026-05-21 13:30:08 +0000

    Release 0.68.4

--- main/settings.py
+++ main/settings.py
@@ -35,7 +35,7 @@ from main.settings_course_etl import *  # noqa: F403
 from main.settings_pluggy import *  # noqa: F403
 from openapi.settings_spectacular import open_spectacular_settings
 
-VERSION = "0.68.3"
+VERSION = "0.68.4"
 
 log = logging.getLogger()
 
