622349bef83ff00a22578f35f6b747f01b7d75d6
Anastasia Beglova <abeglova@mit.edu>
2026-06-09 09:15:37 -0400

    commit

--- frontends/main/src/app/(site)/arithmix/mynumbersClient.ts
+++
@@ -1,7 +0,0 @@
-"use client"
-
-// The mynumbers components use client-only React APIs (e.g. createContext) but
-// the package does not mark them with "use client". Re-exporting them from this
-// "use client" module turns them into client references so they can be rendered
-// from the server-component pages.
-export { Arithmix, ExplainerPage } from "mynumbers"
