6eb839bd3adeaad437d335fede42ae8aa77bf003
Ahtesham Quraish <ahtesham.quraish@192.168.1.6>
2026-06-12 16:07:19 +0500

    fix: Add Article model to Django Admin

--- website_content/admin.py
+++ website_content/admin.py
@@ -1,1 +1,33 @@
-# Register your models here.
+"""Admin configuration for website_content app."""
+
+from django.contrib import admin
+
+from website_content.models import WebsiteContent, WebsiteContentImageUpload
+
+
+@admin.register(WebsiteContent)
+class WebsiteContentAdmin(admin.ModelAdmin):
+    """Admin for WebsiteContent."""
+
+    list_display = (
+        "title",
+        "content_type",
+        "is_published",
+        "publish_date",
+        "user",
+        "created_on",
+    )
+    list_filter = ("content_type", "is_published")
+    search_fields = ("title", "slug", "author_name")
+    readonly_fields = ("slug", "cover_image", "created_on", "updated_on")
+    list_select_related = ("user",)
+    ordering = ("-created_on",)
+
+
+@admin.register(WebsiteContentImageUpload)
+class WebsiteContentImageUploadAdmin(admin.ModelAdmin):
+    """Admin for WebsiteContentImageUpload."""
+
+    list_display = ("user", "created_at")
+    list_select_related = ("user",)
+    ordering = ("-created_at",)
