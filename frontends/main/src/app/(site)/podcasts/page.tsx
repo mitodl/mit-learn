463197103bb0c2b2b23bfd675eb1d7277e13eb79
Ahtesham Quraish <ahtesham.quraish@192.168.1.32>
2026-07-03 15:07:38 +0500

    WIP

---
+++ frontends/main/src/app/(site)/podcasts/page.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Metadata } from "next"
+import { standardizeMetadata } from "@/common/metadata"
+import { PodcastsListingPage } from "@/app-pages/PodcastPage/PodcastsListingPage"
+
+export const metadata: Metadata = standardizeMetadata({
+  title: "MIT Learn | Podcasts",
+})
+
+const Page: React.FC<PageProps<"/podcasts">> = () => {
+  return <PodcastsListingPage />
+}
+
+export default Page
